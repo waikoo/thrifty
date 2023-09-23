d837d223e3c79111c73340fba7dd7b975071cead
waikoo <beroer@protonmail.com>
2023-09-23 22:54:04 +0300

    Switch to internal state from using query params

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,30 +1,21 @@
 "use client";
 import React from 'react'
-import Link from "next/link";
-import { usePathname, useSearchParams } from "next/navigation";
 import { bannerMessages } from "./data";
 import { useMessageDisplay } from "./hooks";
 
 export default function Banner() {
+  const [showBanner, setShowBanner] = React.useState(true);
   const idx = useMessageDisplay(bannerMessages, 5000);
-  const searchParams = useSearchParams();
-  const pathname = usePathname()
 
   return (
     <>
-      {searchParams.get("b") === "true" ? (
-        <section className="grid grid-cols-3 gap-4 py-2 px-3 bg-grey text-bkg">
+      {showBanner ? (
+        <section className="grid grid-cols-3 gap-4 py-1 px-3 bg-grey text-content">
           <span className="col-span-2 col-start-2 col-end-3 justify-self-center">{bannerMessages[idx]}</span>
-          <Link href={{
-            pathname: pathname,
-            query: {
-              b: "false",
-              theme: searchParams.get("theme"),
-              category: searchParams.get("category")
-            }
-          }} className="col-start-3 col-end-4 justify-self-end">
+          <span className="col-start-3 col-end-4 justify-self-end cursor-pointer"
+            onClick={() => setShowBanner(!showBanner)}>
             X
-          </Link>
+          </span>
         </section>
       ) : null}
     </>
