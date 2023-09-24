996b42010758df46564094a7cf5a8afbe324ff03
waikoo <beroer@protonmail.com>
2023-09-24 21:19:59 +0300

    Make server component out of LanguagePicker, remove Header component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,18 +1,21 @@
 import Link from 'next/link'
 import { useTranslation } from '../i18n'
 import { HeroCarousel } from '../components/home'
+import LanguagePicker from '../components/LanguagePicker'
 
 type PageProps = {
   params: {
     locale: string,
-  }
+  },
+  searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export default async function Page({ params: { locale } }: PageProps) {
+export default async function Page({ params: { locale }, searchParams }: PageProps) {
   const { t } = await useTranslation(locale)
 
   return (
     <>
+      <LanguagePicker locale={locale} />
       <HeroCarousel />
       {/* <h1 className="text-4xl font-bold text-content bg-bkg">{t('title')}</h1> */}
       {/* <Link href={`/${locale}/second-page`}> */}
