0bd8f35271e48d4614138630f7b748d053f6f9b3
waikoo <beroer@protonmail.com>
2023-10-03 22:56:10 +0300

    Parents don't resize on h1 textcontent change & remove old dynamic color logic

--- app/components/home/HeroCarousel.tsx
+++ app/components/home/HeroCarousel.tsx
@@ -1,30 +1,29 @@
 "use client"
-import { useSearchParams } from 'next/navigation'
 import React, { useState } from 'react'
 import { HeroCarouselNavigator } from '.'
 
 const HeroCarousel = () => {
-  const params = useSearchParams()
-  const carouselBg = params.get('theme') === 'dark' ? 'bkg' : 'content'
-  const carouselText = params.get('theme') === 'dark' ? 'content' : 'bkg'
   const [selectedCircle, setSelectedCircle] = useState(0);
   const dynamicTitle = selectedCircle === 0 ? 'NEW IN' : selectedCircle === 1 ? 'PROMOS' : 'FILTERS'
-  return (
-    <section className="flex flex-col gap-5">
-      <div className={`flex gap-2 px-12 rounded-3xl m-0 bg-${carouselBg} text-${carouselText} max-w-full`}>
-        <img src="https://picsum.photos/150/220" alt="" className="w-1/3 border" />
-        <img src="https://picsum.photos/150/220" alt="" className="w-1/3 border" />
-        <img src="https://picsum.photos/250/220" alt="" className="w-2/3 border" />
 
-        <article className="flex flex-col justify-around gap-2 py-12 items-end gap-10">
-          <h1 className="vertical-text text-5xl font-bold ml-auto">
+  return (
+    <section className="flex flex-col gap-5 max-h-fit max-w-full">
+      <div className={`flex gap-2 px-12 rounded-[2.8125rem] m-0 bg-content text-bkg max-w-[calc(100% - 28xp)] max-h-fit `}>
+        <div className="bg-green-200 flex flex-8 flex-grow">
+          <img src="https://picsum.photos/150/480" alt="" className="w-1/4 border max-w-full" />
+          <img src="https://picsum.photos/150/480" alt="" className="w-1/4 border max-w-full" />
+          <img src="https://picsum.photos/380/480" alt="" className="w-2/4 border max-w-full" />
+        </div>
+
+        <article className="grid grid-rows-2 items-center py-2 max-h-[100%] max-w-[25%]">
+          <h1 className="vertical-text text-5xl font-bold ml-auto height-half">
             {dynamicTitle}
           </h1>
-          <div className="flex flex-col gap-10 px-8 self-right max-w-xs">
-            <p className="max-w-xs text-xs font-light ml-auto leading-tight ">
+          <div className={`flex flex-col gap-10 max-w-[90%] pr-0 self-center ml-auto height-half max-h-full`}>
+            <p className="text-xs font-light ml-auto min-w-xs pl-auto leading-none">
               Lorem ipsum dolor sit amet consectetur. Vitae orci libero posuere quis elementum non feugiat mi tellus. Nam pellentesque parturient rhoncus odio. Tristique cras cursus in massa nunc tempor adipiscing gravida.
             </p>
-            <button className="border-2 px-5 py-2 text-xs">SHOP NOW</button>
+            <button className="border-2 px-5 py-2 text-xs block">SHOP NOW</button>
           </div>
         </article>
       </div>
