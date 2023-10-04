a7816f190a6b066a7cd46621da700cefd09ec902
waikoo <beroer@protonmail.com>
2023-10-04 11:31:10 +0300

    Fix alignment issues & change placeholder to loremflickr

--- app/components/home/HeroCarousel.tsx
+++ app/components/home/HeroCarousel.tsx
@@ -7,19 +7,19 @@ const HeroCarousel = () => {
   const dynamicTitle = selectedCircle === 0 ? 'NEW IN' : selectedCircle === 1 ? 'PROMOS' : 'FILTERS'
 
   return (
-    <section className="flex flex-col gap-5 max-h-fit max-w-full">
+    <section className="flex flex-col gap-5 max-h-fit max-w-full pt-2">
       <div className={`flex gap-2 px-12 rounded-[2.8125rem] m-0 bg-content text-bkg max-w-[calc(100% - 28xp)] max-h-fit `}>
         <div className="bg-green-200 flex flex-8 flex-grow">
-          <img src="https://picsum.photos/150/480" alt="" className="w-1/4 border max-w-full" />
-          <img src="https://picsum.photos/150/480" alt="" className="w-1/4 border max-w-full" />
-          <img src="https://picsum.photos/380/480" alt="" className="w-2/4 border max-w-full" />
+          <img src="https://loremflickr.com/150/450/clothes,europe/all" alt="" className="w-1/4 border max-w-full" />
+          <img src="https://loremflickr.com/150/450/clothes,europe/all" alt="" className="w-1/4 border max-w-full" />
+          <img src="https://loremflickr.com/380/450/clothes,europe/all" alt="" className="w-2/4 border max-w-full" />
         </div>
 
-        <article className="grid grid-rows-2 items-center py-2 max-h-[100%] max-w-[25%]">
-          <h1 className="vertical-text text-5xl font-bold ml-auto height-half">
+        <article className="grid grid-rows-2 gap-5 py-10 max-h-[100%] max-w-[20%]">
+          <h1 className="vertical-text text-5xl font-bold ml-auto height-half self-end mb-5">
             {dynamicTitle}
           </h1>
-          <div className={`flex flex-col gap-10 max-w-[90%] pr-0 self-center ml-auto height-half max-h-full`}>
+          <div className={`flex flex-col gap-10 max-w-[90%] pr-0 pl-5 ml-auto height-sm max-h-full self-start `}>
             <p className="text-xs font-light ml-auto min-w-xs pl-auto leading-none">
               Lorem ipsum dolor sit amet consectetur. Vitae orci libero posuere quis elementum non feugiat mi tellus. Nam pellentesque parturient rhoncus odio. Tristique cras cursus in massa nunc tempor adipiscing gravida.
             </p>
