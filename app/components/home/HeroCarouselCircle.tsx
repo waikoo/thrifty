657deed98930de24872b435f87a110cd97f2ab78
waikoo <beroer@protonmail.com>
2023-10-04 11:38:00 +0300

    Use refactored colors object & remove obsolete solution

--- app/components/home/HeroCarouselCircle.tsx
+++ app/components/home/HeroCarouselCircle.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
-import { useThemeStore } from '../ThemeToggler'
-import { fill, stroke } from '@/utils/theme'
+import { heroCarouselCircleColors as colors } from '../data'
 
 type HeroCarouselCircleProps = {
   cx: number
@@ -11,7 +9,6 @@ type HeroCarouselCircleProps = {
 }
 
 const HeroCarouselCircle = ({ cx, cy, r, isSelected, onClick }: HeroCarouselCircleProps) => {
-  const theme = useThemeStore((state) => state.theme as 'light' | 'dark')
 
   return (
     <>
@@ -30,8 +27,8 @@ const HeroCarouselCircle = ({ cx, cy, r, isSelected, onClick }: HeroCarouselCirc
         cx={cx}
         cy={cy}
         r={r}
-        fill={isSelected ? fill.selected[theme] : fill.unselected[theme]}
-        stroke={stroke[theme]}
+        fill={isSelected ? colors.fill.selected[theme] : colors.fill.unselected[theme]}
+        stroke={colors.stroke[theme]}
         className="cursor-pointer"
         style={{ pointerEvents: "none" }}
       />
