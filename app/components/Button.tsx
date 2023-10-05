55b97d5ca8928c3a02df4385a22736ae3d41ea08
waikoo <beroer@protonmail.com>
2023-10-05 23:58:29 +0300

    Make component more versatile to handle the needs of different instances

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -3,18 +3,41 @@ import { twMerge as tm } from 'tailwind-merge'
 type ButtonProps = {
   children: React.ReactNode;
   className?: string;
-  inverse?: boolean; 
-  border?: boolean; 
+  inverse?: boolean;
+  border?: boolean;
+  selected?: boolean;
+  setSelected?: (newState: 'login' | 'signup') => void
+  signUp?: () => void
 }
 
-const Button = ({children, className, inverse, border}: ButtonProps) => {
+const Button = ({ children, className, inverse, border, selected, setSelected, signUp }: ButtonProps) => {
+  const newState: 'login' | 'signup' = ((children as string).replace(' ', '').toLowerCase() as 'login' | 'signup')
+  const signInOrLoginHandler = () => {
+
+    if (selected || !setSelected) return
+    setSelected(newState)
+  }
+
+  const signUpHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (signUp) {
+      try {
+        signUp()
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
   return (
     <button className={
-      tm("bg-content text-bkg block w-full p-2 border-bkg", 
-      className, 
-      inverse && "bg-bkg text-content border-content",
-      border && "border-[0.1rem]"
-     )}
+      tm("bg-content text-bkg block w-full p-2 border-bkg",
+        className,
+        inverse && "bg-bkg text-content border-content",
+        border && "border-[0.1rem]",
+        selected && "bg-content text-bkg",
+        !selected && "bg-bkg text-content"
+      )}
+      onClick={selected || setSelected ? signInOrLoginHandler : (e) => signUpHandler(e)}
     >{children}
     </button>
   )
