5cae91ace3542003584af1f35710ae21b64b7c14
waikoo <beroer@protonmail.com>
2023-10-08 23:49:08 +0300

    Add menuItems

--- app/components/data/index.ts
+++ app/components/data/index.ts
@@ -1,4 +1,5 @@
 export { bannerMessages } from "./bannerMessages";
 export { colors } from "./home";
 export { heroCarouselCircleColors, themeSettings } from "./theme"
+export { menuItems } from "./navigation"
 
