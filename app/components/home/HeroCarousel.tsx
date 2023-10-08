b52dd6aaf710275ff46e7bc92ccc1cb5f7e4dc97
waikoo <beroer@protonmail.com>
2023-10-08 23:43:34 +0300

    Replace CTA button with new instance

--- app/components/home/HeroCarousel.tsx
+++ app/components/home/HeroCarousel.tsx
@@ -1,7 +1,7 @@
 "use client"
-import React, { useState } from 'react'
-import { HeroCarouselNavigator } from '.'
-import  Button  from '../Button'
+import { useState } from 'react';
+import { HeroCarouselNavigator } from '.';
+import { SubmitButton } from '..';
 
 const HeroCarousel = () => {
   const [selectedCircle, setSelectedCircle] = useState(0);
@@ -24,7 +24,7 @@ const HeroCarousel = () => {
             <p className="text-xs font-light ml-auto min-w-xs pl-auto leading-none">
               Lorem ipsum dolor sit amet consectetur. Vitae orci libero posuere quis elementum non feugiat mi tellus. Nam pellentesque parturient rhoncus odio. Tristique cras cursus in massa nunc tempor adipiscing gravida.
             </p>
-            <Button border className="font-bold border-[0.3]">SHOP NOW</Button>
+            <SubmitButton className="font-bold border-[0.2rem] border-bkg">SHOP NOW</SubmitButton>
           </div>
         </article>
       </div>
