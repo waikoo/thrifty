8dac43531868add3c6183fc6b8ca1e13d21160f7
waikoo <beroer@protonmail.com>
2023-10-09 00:11:31 +0300

    Fix up text

--- app/components/home/ColorCarousel.tsx
+++ app/components/home/ColorCarousel.tsx
@@ -3,9 +3,13 @@ import { colors } from '../data'
 
 export default function ColorCarousel() {
   return (
-    <section className="flex flex-col gap-5 items-center mt-15">
-      <h2 className="text-content font-bold pt-15">DISCOVER YOUR PERFECT PALETTE</h2>
-      <span>Shop by Your Favorite Colors and Unleash Your Style</span>
+    <section className="flex flex-col gap-5 items-center mt-12">
+
+      <div className="flex flex-col items-center">
+        <h2 className="text-content font-semibold text-[1.875rem] my-0 tracking-[0.16875em]">DISCOVER YOUR PERFECT PALETTE</h2>
+        <span className="text-[0.75rem] font-medium tracking-[0.07875rem] my-0">Shop by Your Favorite Colors and Unleash Your Style</span>
+      </div>
+
       <div className="grid grid-cols-4 gap-5 rounded-2xl p-5">
         {colors.map(color => (
           <ColorCarouselElement
