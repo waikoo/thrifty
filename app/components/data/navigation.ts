814536aa5366e177773b7928535c1a1795a84d6a
waikoo <beroer@protonmail.com>
2023-10-08 23:35:29 +0300

    Extract menuItem data to mappable array of objects

---
+++ app/components/data/navigation.ts
@@ -0,0 +1,40 @@
+import { IconMenuAccount, IconMenuContact, IconMenuHelp, IconMenuLogOut, IconMenuOrders, IconMenuReturns, IconMenuSettings } from "../navigation/icons/menu";
+
+
+export const menuItems = [
+  {
+    content: "My Account",
+    href: "#",
+    Img: IconMenuAccount,
+  },
+  {
+    content: "My Returns",
+    href: "#",
+    Img: IconMenuReturns,
+  },
+  {
+    content: "My Orders",
+    href: "#",
+    Img: IconMenuOrders,
+  },
+  {
+    content: "Settings",
+    href: "#",
+    Img: IconMenuSettings,
+  },
+  {
+    content: "Help",
+    href: "#",
+    Img: IconMenuHelp,
+  },
+  {
+    content: "Contact",
+    href: "#",
+    Img: IconMenuContact,
+  },
+  {
+    content: "Log Out",
+    href: "#",
+    Img: IconMenuLogOut,
+  }
+]
