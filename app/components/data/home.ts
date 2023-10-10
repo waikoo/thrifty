c9109a4ef7d9bbe7e9e0d903f7df0afc932744b2
waikoo <beroer@protonmail.com>
2023-10-10 22:28:01 +0300

    Add HeroCarousel photos

--- app/components/data/home.ts
+++ app/components/data/home.ts
@@ -1,12 +1,32 @@
-import newIn_01 from '@/public/home/new_in_01.jpg';
-import newIn_02 from '@/public/home/new_in_02.jpg';
-import newIn_03 from '@/public/home/new_in_03.jpg';
-import promos_01 from '@/public/home/promos_01.jpg';
-import promos_02 from '@/public/home/promos_02.jpg';
-import promos_03 from '@/public/home/promos_03.jpg'
-import filters_01 from '@/public/home/filters_01.jpg'
-import filters_02 from '@/public/home/filters_02.jpg'
-import filters_03 from '@/public/home/filters_03.jpg'
+import men_newIn_01 from '@/public/home/men_new_in_01.jpg';
+import men_newIn_02 from '@/public/home/men_new_in_02.jpg';
+import men_newIn_03 from '@/public/home/men_new_in_03.jpg';
+import men_promos_01 from '@/public/home/men_promos_01.jpg';
+import men_promos_02 from '@/public/home/men_promos_02.jpg';
+import men_promos_03 from '@/public/home/men_promos_03.jpg'
+import men_filters_01 from '@/public/home/men_filters_01.jpg'
+import men_filters_02 from '@/public/home/men_filters_02.jpg'
+import men_filters_03 from '@/public/home/men_filters_03.jpg'
+
+import women_newIn_01 from '@/public/home/women_new_in_01.jpg';
+import women_newIn_02 from '@/public/home/women_new_in_02.jpg';
+import women_newIn_03 from '@/public/home/women_new_in_03.jpg';
+import women_promos_01 from '@/public/home/women_promos_01.jpg';
+import women_promos_02 from '@/public/home/women_promos_02.jpg';
+import women_promos_03 from '@/public/home/women_promos_03.jpg'
+import women_filters_01 from '@/public/home/women_filters_01.jpg'
+import women_filters_02 from '@/public/home/women_filters_02.jpg'
+import women_filters_03 from '@/public/home/women_filters_03.jpg'
+
+import kids_newIn_01 from '@/public/home/kids_new_in_01.jpg';
+import kids_newIn_02 from '@/public/home/kids_new_in_02.jpg';
+import kids_newIn_03 from '@/public/home/kids_new_in_03.jpg';
+import kids_promos_01 from '@/public/home/kids_promos_01.jpg';
+import kids_promos_02 from '@/public/home/kids_promos_02.jpg';
+import kids_promos_03 from '@/public/home/kids_promos_03.jpg'
+import kids_filters_01 from '@/public/home/kids_filters_01.jpg'
+import kids_filters_02 from '@/public/home/kids_filters_02.jpg'
+import kids_filters_03 from '@/public/home/kids_filters_03.jpg'
 
 import { TColor, Timages } from "@/types/home";
 
@@ -36,16 +56,53 @@ export const colors: TColor[] = [{
 }]
 
 export const images: Timages = {
-  small: {
-    0: [newIn_01, newIn_02],
-    1: [promos_01, promos_02],
-    2: [filters_01, filters_02]
+  men: {
+    small: {
+      0: [men_newIn_01, men_newIn_02],
+      1: [men_promos_01, men_promos_02],
+      2: [men_filters_01, men_filters_02]
+    },
+    large: {
+      0: men_newIn_03,
+      1: men_promos_03,
+      2: men_filters_03
+    }
   },
-  large: {
-    0: newIn_03,
-    1: promos_03,
-    2: filters_03
-  }
+  women: {
+    small: {
+      0: [women_newIn_01, women_newIn_02],
+      1: [women_promos_01, women_promos_02],
+      2: [women_filters_01, women_filters_02]
+    },
+    large: {
+      0: women_newIn_03,
+      1: women_promos_03,
+      2: women_filters_03
+    }
+  },
+  kids: {
+    small: {
+      0: [kids_newIn_01, kids_newIn_02],
+      1: [kids_promos_01, kids_promos_02],
+      2: [kids_filters_01, kids_filters_02]
+    },
+    large: {
+      0: kids_newIn_03,
+      1: kids_promos_03,
+      2: kids_filters_03
+    }
+  },
 }
+//
+// small: {
+//   0: [newIn_01, newIn_02],
+//   1: [promos_01, promos_02],
+//   2: [filters_01, filters_02]
+// },
+// large: {
+//   0: newIn_03,
+//   1: promos_03,
+//   2: filters_03
+// }
 
 
