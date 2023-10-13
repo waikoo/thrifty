14b915f90abef62dc5d53268349d746da92989d9
waikoo <beroer@protonmail.com>
2023-10-13 19:30:54 +0300

    Add state & setter for password recovery component

--- state/uiState.ts
+++ state/uiState.ts
@@ -3,15 +3,19 @@ import { create } from 'zustand'
 type UIState = {
   showSignIn: boolean
   showMyAccount: boolean
+  showRecovery: boolean
   setShowSignIn: (value: boolean | (boolean)) => void
   setShowMyAccount: (value: boolean | (boolean)) => void
+  setShowRecovery: (value: boolean | (boolean)) => void
 }
 
 export const useUIStore = create<UIState>((set) => ({
   showSignIn: false,
   showMyAccount: false,
+  showRecovery: false,
   setShowMyAccount: (value) => set({ showMyAccount: value }),
   setShowSignIn: (value) => set({ showSignIn: value }),
+  setShowRecovery: (value) => set({ showRecovery: value })
 }))
 
 type HeroCarouselState = {
