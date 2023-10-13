2dfaf98e01e33c536a04b456dc187a4b309a542e
waikoo <beroer@protonmail.com>
2023-10-13 18:19:46 +0300

    Increase padding

--- app/components/StateButton.tsx
+++ app/components/StateButton.tsx
@@ -4,7 +4,7 @@ import { twMerge as tm } from 'tailwind-merge'
 export default function StateButton({ children, className, selected, authValue, setSelected }: StateButtonProps) {
 
   return (
-    <button className={tm("bg-content text-bkg block w-full p-2 border-bkg",
+    <button className={tm("bg-content text-bkg block w-full p-4 border-bkg",
       className,
       !selected && "bg-bkg text-content border-[0.1rem] border-content")
     }
