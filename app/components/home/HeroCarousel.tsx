b9d9f32e4969727c1154cabb898235ae46813c05
waikoo <beroer@protonmail.com>
2023-10-13 19:06:07 +0300

    Remove padding

--- app/components/home/HeroCarousel.tsx
+++ app/components/home/HeroCarousel.tsx
@@ -5,9 +5,9 @@ import { HeroCarouselArticle, HeroCarouselImages, HeroCarouselNavigator } from '
 const HeroCarousel = ({ category }: Category) => {
 
   return (
-    <section className="flex flex-col gap-5 max-w-full pt-2">
+    <section className="flex flex-col gap-5 max-w-full">
 
-      <div className={`flex flex-row gap-2 px-12 rounded-[2.8125rem] m-0 bg-content text-bkg max-w-full aspect-ratio max-h-[80vh]`}>
+      <div className={`flex flex-row gap-2 px-20 rounded-[2.8125rem] m-0 bg-content text-bkg max-w-full aspect-ratio max-h-[39.375rem]`}>
         <HeroCarouselImages category={category} />
         <HeroCarouselArticle />
       </div>
