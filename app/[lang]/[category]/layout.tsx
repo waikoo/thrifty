848a2b3475423f1c8d29a3e991642af808887fd3
waikoo <beroer@protonmail.com>
2023-10-13 17:16:32 +0300

    Change font to Inter

--- app/[lang]/[category]/layout.tsx
+++ app/[lang]/[category]/layout.tsx
@@ -4,7 +4,9 @@ import { Banner } from "@/app/components/generic";
 import { LanguagePicker, Navigation } from "@/app/components/navigation";
 import "@/styles/styles.css";
 import { Suspense } from "react";
+import { Inter } from 'next/font/google'
 
+const inter = Inter({ subsets: ['latin'], display: 'swap' })
 export const metadata = {
   title: 'Thrifty'
 }
@@ -18,13 +20,13 @@ export type RootLayoutProps = {
 }
 
 export default function RootLayout({ children, params: { lang, category } }: RootLayoutProps) {
-  console.log(lang, category)
+
   return (
-    <html data-theme={themeSettings.DEFAULT_THEME}>
+    <html data-theme={themeSettings.DEFAULT_THEME} className={inter.className}>
       <head />
       <body className="relative bg-bkg">
         <Banner />
-        <section className="h-screen bg-bkg text-content flex flex-col px-10">
+        <section className="h-screen bg-bkg text-content flex flex-col px-20">
           <LanguagePicker {... { lang, category }} />
           <Navigation />
           <main className="mt-5">
