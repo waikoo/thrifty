516420c7eb449448e6bdd6fead2b438c26b9f3e7
waikoo <beroer@protonmail.com>
2023-10-13 19:03:58 +0300

    Remove colors - got its own module

--- app/components/data/index.ts
+++ app/components/data/index.ts
@@ -1,4 +1,4 @@
 export { bannerMessages } from "./bannerMessages";
-export { colors, HERO_CAROUSEL_TITLES } from "./home";
+export { HERO_CAROUSEL_TITLES } from "./home";
 export { heroCarouselCircleColors, themeSettings } from "./theme"
 export { menuItems } from "./navigation"
