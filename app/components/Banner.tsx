b4a64be6c405eb88139fb582eca330617b36af85
waikoo <beroer@protonmail.com>
2023-10-14 21:55:26 +0300

    Extract local state to zustand store

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,21 +1,26 @@
 "use client";
-import React from 'react'
+import { useUIStore } from '@/state';
 import { bannerMessages } from "./data";
 import { useMessageDisplay } from "./hooks";
 
 export default function Banner() {
-  const [showBanner, setShowBanner] = React.useState(true);
+  const { showBanner, setShowBanner } = useUIStore()
   const idx = useMessageDisplay(bannerMessages, 5000);
 
   return (
     <>
       {showBanner ? (
-        <section className={`grid grid-cols-3 gap-4 py-1 bg-faded px-3 text-black-500`}>
-          <span className="col-span-2 col-start-2 col-end-3 justify-self-center">{bannerMessages[idx]}</span>
+        <section className={`grid grid-cols-3 gap-4 py-1 bg-faded px-3 text-black w-full`}>
+
+          <span className="col-span-2 col-start-2 col-end-3 justify-self-center">
+            {bannerMessages[idx]}
+          </span>
+
           <span className="col-start-3 col-end-4 justify-self-end cursor-pointer"
             onClick={() => setShowBanner(!showBanner)}>
             X
           </span>
+
         </section>
       ) : null}
     </>
