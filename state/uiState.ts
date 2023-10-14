f15ac1ee96167920d809175e18e8439ec30cc7d7
waikoo <beroer@protonmail.com>
2023-10-14 22:38:44 +0300

    Add Banner & BackToTop states

--- state/uiState.ts
+++ state/uiState.ts
@@ -4,18 +4,26 @@ type UIState = {
   showSignIn: boolean
   showMyAccount: boolean
   showRecovery: boolean
+  showBanner: boolean
+  showBackToTop: boolean
   setShowSignIn: (value: boolean | (boolean)) => void
   setShowMyAccount: (value: boolean | (boolean)) => void
   setShowRecovery: (value: boolean | (boolean)) => void
+  setShowBanner: (value: boolean | (boolean)) => void
+  setShowBackToTop: (value: boolean | (boolean)) => void
 }
 
 export const useUIStore = create<UIState>((set) => ({
   showSignIn: false,
   showMyAccount: false,
   showRecovery: false,
+  showBanner: true,
+  showBackToTop: false,
   setShowMyAccount: (value) => set({ showMyAccount: value }),
   setShowSignIn: (value) => set({ showSignIn: value }),
-  setShowRecovery: (value) => set({ showRecovery: value })
+  setShowRecovery: (value) => set({ showRecovery: value }),
+  setShowBanner: (value) => set({ showBanner: value }),
+  setShowBackToTop: (value) => set({ showBackToTop: value }),
 }))
 
 type HeroCarouselState = {
