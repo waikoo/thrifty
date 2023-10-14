82532522741fc776423fa57d13fdf122ab19ff99
waikoo <beroer@protonmail.com>
2023-10-14 22:22:52 +0300

    Relocate Banner to /navigation

--- app/components/navigation/Banner.tsx
+++ app/components/navigation/Banner.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { useUIStore } from '@/state';
-import { bannerMessages } from "./data";
-import { useMessageDisplay } from "./hooks";
+import { bannerMessages } from '../data';
+import useMessageDisplay from '../hooks/useMessageDisplay';
 
 export default function Banner() {
   const { showBanner, setShowBanner } = useUIStore()
   const idx = useMessageDisplay(bannerMessages, 5000);
+  const textColor = showBanner ? 'text-black' : 'text-content'
 
   return (
     <>
       {showBanner ? (
-        <section className={`grid grid-cols-3 gap-4 py-1 bg-faded px-3 text-black w-full`}>
+        <section className={`grid grid-cols-3 gap-4 py-1 bg-faded px-3 text-black-500 w-full
+          ${textColor}`}>
 
           <span className="col-span-2 col-start-2 col-end-3 justify-self-center">
             {bannerMessages[idx]}
