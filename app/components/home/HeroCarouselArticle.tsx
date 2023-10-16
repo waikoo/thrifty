00561553209da9327b7525294f7079be14276498
waikoo <beroer@protonmail.com>
2023-10-16 18:42:35 +0300

    Improve overflow on smaller viewports

--- app/components/home/HeroCarouselArticle.tsx
+++ app/components/home/HeroCarouselArticle.tsx
@@ -6,18 +6,18 @@ export default function HeroCarouselArticle() {
   const { dynamicTitle } = useHeroCarousel(HERO_CAROUSEL_TITLES)
 
   return (
-    <article className="grid grid-rows-2 gap-5 py-10 h-auto">
+    <article className="ml-6 grid auto-rows-min grid-rows-2 gap-5 py-6 sm:gap-0 md:gap-2">
 
-      <h1 className="hero-h1 ml-auto self-end my-auto">
+      <h1 className="hero-h1 my-auto ml-auto self-end text-lg sm:text-3xl md:self-start md:text-5xl lg:text-[4rem]">
         {dynamicTitle}
       </h1>
 
-      <div className={`flex flex-col gap-10 max-w-[90%] pr-0 pl-5 ml-auto height-sm max-h-full self-start `}>
-        <p className="hero-p ml-auto min-w-xs pl-auto leading-none">
+      <div className={`flex flex-col gap-0 sm:gap-0 md:gap-4 lg:gap-10  max-w-[90%] pr-0 pl-5 ml-auto self-start `}>
+        <p className="hero-p pl-auto ml-auto leading-none sm:text-xs">
           Lorem ipsum dolor sit amet consectetur. Vitae orci libero posuere quis elementum non feugiat mi tellus. Nam pellentesque parturient rhoncus odio. Tristique cras cursus in massa nunc tempor adipiscing gravida.
         </p>
 
-        <SubmitButton className="font-bold border-[0.2rem] border-bkg">SHOP NOW</SubmitButton>
+        <SubmitButton hero className="border-bkg whitespace-nowrap border-[0.2rem] font-bold">SHOP NOW</SubmitButton>
 
       </div>
 
