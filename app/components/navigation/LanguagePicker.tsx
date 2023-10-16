c1dcaad08dfbc86dc5bb7d263c8d0f5f460dd21c
waikoo <beroer@protonmail.com>
2023-10-16 18:46:52 +0300

    Add lang attribute

--- app/components/navigation/LanguagePicker.tsx
+++ app/components/navigation/LanguagePicker.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useUIStore } from "@/state";
+import { usePathname } from "next/navigation";
 import { LanguageElement } from ".";
 
 export type LanguagePickerParams = {
@@ -7,12 +8,13 @@ export type LanguagePickerParams = {
 
 const LanguagePicker = () => {
   const locales = ['en', 'de'];
+  const [_, lang, category] = usePathname()
   const { showBanner } = useUIStore()
   const textColor = showBanner ? 'text-black' : 'text-content'
   const top = showBanner ? 'top-0' : 'top-5'
 
   return (
-    <nav className={`flex items-center bg-bkg gap-3 bg-transparent col-start-1 col-end-2 hover:cursor-pointer absolute left-2 p-1 
+    <nav lang={lang} className={`flex items-center bg-bkg gap-3 bg-transparent col-start-1 col-end-2 hover:cursor-pointer absolute left-2 p-1 z-50
       ${top} ${textColor}`}>
 
       {locales.map((locale: string) => {
