b6fa74965f1b9ae8feca5d6f1a233b3267e4b330
waikoo <beroer@protonmail.com>
2023-10-16 23:10:36 +0300

    Click & drag works once

--- app/components/home/ColorCarouselImages.tsx
+++ app/components/home/ColorCarouselImages.tsx
@@ -1,20 +1,60 @@
 "use client"
-import { useUIStore } from "@/state/uiState";
+import { useColorCarouselRef } from "@/state/colorCarouselState";
 import { TColor } from "@/types/home";
+import { useEffect, useRef } from "react";
 import { ColorCarouselElement } from ".";
+import { useEventListener } from "../hooks";
 
 type ColorCarouselImagesProps = {
   colors: TColor[]
 }
 
 export default function ColorCarouselImages({ colors }: ColorCarouselImagesProps) {
-  const { isSecondColorPage } = useUIStore()
-  const start = isSecondColorPage ? 4 : 0
-  const end = isSecondColorPage ? colors.length : 4
+  const imagesRef = useRef<HTMLDivElement>(null)
+  const { containerRef, setContainerRef } = useColorCarouselRef()
+  let initialX = 0;
+
+  const handleScrolling = (e: MouseEvent) => {
+    const currentX = e.clientX
+
+    if (containerRef) {
+      const imageWidth = containerRef.clientWidth;
+      containerRef.scrollTo({
+        left: currentX < initialX ? containerRef.scrollLeft - imageWidth : containerRef.scrollLeft + imageWidth,
+        behavior: 'smooth',
+      });
+    }
+    console.log('handleScrolling triggered')
+  }
+
+  useEffect(() => {
+    setContainerRef(imagesRef.current)
+  }, [])
+
+  useEventListener({
+    eventType: "mousedown",
+    listener: (e) => {
+      console.log('mousedown occurred')
+      initialX = (e as MouseEvent).clientX
+    },
+    target: imagesRef.current,
+  })
+
+  useEventListener({
+    eventType: "dragstart",
+    listener: (e) => {
+      console.log('dragstart occurred')
+      handleScrolling(e as MouseEvent)
+    },
+    target: imagesRef.current,
+  })
+
+
 
   return (
-    <div className="flex w-full gap-6 overflow-x-auto">
-      {colors.slice(start, end).map(({ id, color, imgUrl, alt }: TColor) => (
+    <div ref={imagesRef} draggable={false}
+      className="noscrollbar  grid select-none snap-x snap-mandatory auto-cols-[23%] grid-flow-col gap-[30px] overflow-x-auto overscroll-x-contain">
+      {colors.map(({ id, color, imgUrl, alt }: TColor) => (
         <ColorCarouselElement
           key={id}
           color={color}
@@ -25,4 +65,3 @@ export default function ColorCarouselImages({ colors }: ColorCarouselImagesProps
     </div>
   );
 }
-
