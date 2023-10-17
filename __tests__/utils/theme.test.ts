a663b667568d922fc3dbb93fa7fb449f36f343bb
waikoo <beroer@protonmail.com>
2023-10-17 19:10:26 +0300

    Remove testing

--- __tests__/utils/theme.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "bun:test"
-import { getSvgColor } from "@/utils/theme"
-
-test("Get the color of svgs for different themes", () => {
-  expect(getSvgColor('light')).toEqual('#191A1A')
-  expect(getSvgColor('dark')).toEqual('#fff')
-})
