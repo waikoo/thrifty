6597d95820218e780d2db186bafb8706d94f3441
waikoo <beroer@protonmail.com>
2023-10-17 21:25:07 +0300

    Simplify logic & extract conditional classes, refactor markup

--- app/components/generic/ThemeToggler.tsx
+++ app/components/generic/ThemeToggler.tsx
@@ -1,40 +1,39 @@
 "use client";
-import { useThemeStore } from "@/state/themeState";
-import { useEffect, useState } from "react";
+import { useDarkMode } from "../hooks/";
+import { useTogglerStyles } from "../hooks/";
 
 const ThemeToggler = () => {
-  const theme = useThemeStore((state) => state.theme)
-  const updateTheme = useThemeStore((state) => state.updateTheme)
-  const [nextThemeText, setNextThemeText] = useState<'Light' | 'Dark' | null>(null)
-  const LIGHT = theme === 'dark' ? 'text-gray-500 opacity-70' : 'text-black'
-  const DARK = theme === 'dark' ? 'text-black' : 'text-gray-500 opacity-70'
-
-  const handleToggleTheme = () => {
-    const newTheme = document.documentElement.dataset.theme === 'dark' ? 'light' : 'dark'
-
-    document.documentElement.dataset.theme = newTheme
-    updateTheme(newTheme)
-  }
-
-  useEffect(() => {
-    setNextThemeText(theme === 'dark' ? 'Light' : 'Dark')
-  }, [theme])
+  const { isDark, handleToggleTheme } = useDarkMode()
+  const { getStyleForElement, bgColor, circleColor, xPos } = useTogglerStyles();
 
   return (
     <button
       className="flex cursor-pointer items-center gap-2 justify-self-end"
-      onClick={handleToggleTheme}>
+      onClick={handleToggleTheme}
+    >
+      <span className={`${getStyleForElement('light')}`} >
+        LIGHT
+      </span>
 
-      <span className={`${LIGHT}`}>LIGHT</span>
+      <div className={`relative grid h-[1.4rem] w-[2.5rem] items-center rounded-full 
+        ${bgColor}`}>
 
-      <div className={`relative grid h-[1.4rem] w-[2.5rem] items-center rounded-full ${theme !== 'dark' ? 'bg-black' : 'bg-white'} `}>
-
-        <div className={`absolute h-[0.8rem] w-[0.8rem] rounded-full ${theme !== 'dark' ? 'bg-white' : 'bg-black'} transition-all ${theme === 'dark' ? 'translate-x-[1.4rem]' : 'translate-x-[0.3rem]'}`}></div>
+        <div className={`absolute h-[0.8rem] w-[0.8rem] rounded-full transition-all 
+          ${circleColor} ${xPos}`}>
+        </div>
 
       </div>
-      <input type="checkbox" checked={theme === 'dark'} className="sr-only" />
 
-      <span className={`${DARK}`}>DARK</span>
+      <input type="checkbox"
+        checked={isDark}
+        className="sr-only"
+        id="theme-toggler"
+        onChange={() => { }}
+      />
+
+      <span className={`${getStyleForElement('dark')}`} >
+        DARK
+      </span>
 
     </button>
   );
