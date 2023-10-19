667b18f4b295df1a326da5b7e27536c53647f63e
waikoo <beroer@protonmail.com>
2023-10-19 21:29:46 +0300

    Remove ThemeToggler

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,7 +1,6 @@
 export { default as SubmitButton } from "./SubmitButton";
 export { default as SignInOrUp } from "./SignInOrUp";
 export { default as StateButton } from "./StateButton";
-export { default as ThemeToggler } from "./ThemeToggler";
 export { default as Spinner } from "./Spinner";
 export { default as CheckBox } from "./CheckBox"
 export { default as Error } from "./Error"
