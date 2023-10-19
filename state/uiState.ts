6e5228f511355087dadcbbf3d97b2be4e10877ce
waikoo <beroer@protonmail.com>
2023-10-19 21:37:35 +0300

    Add category state modifiers

--- state/uiState.ts
+++ state/uiState.ts
@@ -1,3 +1,4 @@
+import Category from '@/app/components/navigation/Category'
 import { create } from 'zustand'
 
 type UIState = {
@@ -8,6 +9,8 @@ type UIState = {
   showBackToTop: boolean
   isSecondColorPage: boolean
   showCategoryMenu: boolean
+  category: Category['category'] | null
+  setCategory: (value: Category['category']) => void
   setShowSignIn: (value: boolean | (boolean)) => void
   setShowMyAccount: (value: boolean | (boolean)) => void
   setShowRecovery: (value: boolean | (boolean)) => void
@@ -25,6 +28,8 @@ export const useUIStore = create<UIState>((set) => ({
   showBackToTop: false,
   isSecondColorPage: false,
   showCategoryMenu: false,
+  category: null,
+  setCategory: (value) => set({ category: value }),
   setShowMyAccount: (value) => set({ showMyAccount: value }),
   setShowSignIn: (value) => set({ showSignIn: value }),
   setShowRecovery: (value) => set({ showRecovery: value }),
