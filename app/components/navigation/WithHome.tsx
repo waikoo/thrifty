a64896fd3b9301544fd55701a88742bef1b65e87
waikoo <beroer@protonmail.com>
2023-10-19 21:36:27 +0300

    Add category popup disappearing after hovering over logo

--- app/components/navigation/WithHome.tsx
+++ app/components/navigation/WithHome.tsx
@@ -1,5 +1,8 @@
+"use client"
+import { useUIStore } from "@/state";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useRef } from "react";
 
 type withHomeProps = {
   children: React.ReactNode
@@ -7,9 +10,22 @@ type withHomeProps = {
 
 export default function WithHome({ children }: withHomeProps) {
   const [, locale, category] = usePathname().split('/')
+  const anchorRef = useRef<HTMLAnchorElement | null>(null)
+  const { setShowCategoryMenu } = useUIStore()
+
+  const handleMouseEnter = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (e.target === anchorRef.current) {
+      setShowCategoryMenu(false)
+    }
+  }
 
   return (
-    <Link href={`/${locale}/${category}`} className="flex justify-center">
+    <Link
+      href={`/${locale}/${category}`}
+      className="flex justify-center"
+      ref={anchorRef}
+      onMouseEnter={handleMouseEnter}
+    >
       {children}
     </Link>
   )
