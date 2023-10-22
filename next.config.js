e504185ca72fa0b9eeb68d1e9c71d3676b202b72
waikoo <beroer@protonmail.com>
2023-10-22 20:10:19 +0300

    Add unsplash as image domain

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // allows to pass serverasync functions to client components with the "use server" directive inside them
+  images: {
+    domains: ['images.unsplash.com', 'plus.unsplash.com'],
+  },
   experimental: {
-    // serverActions: true,
+    serverActions: true,
   },
 };
 
