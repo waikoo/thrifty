59bd2b02804a5729c4e2675e704b845a607f05b1
waikoo <beroer@protonmail.com>
2023-10-23 23:38:10 +0300

    Make PopularBrands component

---
+++ app/components/data/popularBrandsImagesSrc.ts
@@ -0,0 +1,5 @@
+export const images = [
+  "https://images.unsplash.com/photo-1508599589920-14cfa1c1fe4d?auto=format&fit=crop&q=80&w=1103&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  'https://images.unsplash.com/photo-1558452919-08ae4aea8e29?auto=format&fit=crop&q=80&w=988&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ',
+  "https://images.unsplash.com/photo-1557167668-6eb71e76b603?auto=format&fit=crop&q=80&w=1170&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D "
+]
