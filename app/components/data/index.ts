f802718916eb9f22e2d36c3c730bb3e308ea6168
waikoo <beroer@protonmail.com>
2023-10-23 23:35:32 +0300

    Add popularBrandsImagesSrc

--- app/components/data/index.ts
+++ app/components/data/index.ts
@@ -3,3 +3,4 @@ export { HERO_CAROUSEL_TITLES } from "./home";
 export { heroCarouselCircleColors, themeSettings } from "./theme"
 export { menuItems } from "./navigation"
 export { images } from "./apparelImagesSrc"
+export { images as brandImages } from "./popularBrandsImagesSrc"
