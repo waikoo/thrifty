25563a00e24c968fc3393293298488023f92756c
waikoo <beroer@protonmail.com>
2023-10-25 23:35:19 +0300

    Fix component aligning with bottom of page

--- app/components/home/HeroCarousel.tsx
+++ app/components/home/HeroCarousel.tsx
@@ -11,7 +11,7 @@ const HeroCarousel = ({ category, lang }: HeroCarouselProps) => {
   return (
     <section className="flex max-w-full flex-col gap-5">
 
-      <div className={`flex flex-row gap-2 px-20 rounded-[2.8125rem] m-0 bg-content text-bkg max-w-full`}>
+      <div className={`flex flex-row gap-2 px-20 rounded-[2.8125rem] m-0 bg-content text-bkg max-w-full h-[75vh]`}>
         <HeroCarouselImages {... { category }} />
         <HeroCarouselArticle {... { lang }} />
       </div>
