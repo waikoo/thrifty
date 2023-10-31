f6370542d8bd183ab762d0f48bf0303cc781b189
waikoo <beroer@protonmail.com>
2023-10-31 23:09:25 +0200

    Delete comments

--- app/components/admin/ProductImage.tsx
+++ app/components/admin/ProductImage.tsx
@@ -8,12 +8,10 @@ type ProductImageProps = {
 
 export default function ProductImage({ }: ProductImageProps) {
   const { imgUrl, setImgUrl } = useProductStore()
-
-  // const [images, setImages] = useState<string[]>([])
   const inputRef = useRef<HTMLInputElement | null>(null)
 
   return (
-    <div className="flex w-full flex-col items-center gap-6">
+    <div className="flex w-[50%] flex-col items-center gap-6">
 
       <div className="flex w-full items-center justify-center gap-6">
         <span className="text-bold">URL</span>
@@ -22,13 +20,11 @@ export default function ProductImage({ }: ProductImageProps) {
           <input ref={inputRef} type="text" className={`adminBorder w-full p-[0.5rem] bg-bkg`} placeholder="Add link here" />
 
           <ImageButton {... { inputRef }} />
-          {/* <ImageButton {... { inputRef, images, setImages }} /> */}
         </div>
 
       </div>
 
       <ImagesDisplay />
-      {/* <ImagesDisplay {... { images }} /> */}
 
     </div>
   )
