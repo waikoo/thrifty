eced8c11eec5cc54a82a16fbd214243f456c5bab
waikoo <beroer@protonmail.com>
2023-11-02 15:23:00 +0200

    Simplify Button component = encapsulate logic into saveDraft

--- app/components/admin/Button.tsx
+++ app/components/admin/Button.tsx
@@ -1,36 +1,15 @@
 "use client"
-import { supabase } from "@/app/supabase"
 import { useProductStore } from "@/state/productState"
 
 export default function Button() {
-  const state = useProductStore(state => state)
-  const { counter, setCounter, saveDraft } = useProductStore()
-
-  const handleSave = () => {
-    console.log(state)
-    saveDraft()
-    setCounter({ created: counter.created ? counter.created + 1 : 1 })
-
-    supabase.auth.getUser().then(({ data: { user } }) => {
-      console.log(user)
-    })
-    // supabase.auth.getSession().then(({ data: { session } }) => {
-    //   console.log(session)
-    // })
-  }
-  supabase.auth.getSession().then(({ data }) => {
-    console.log(data)
-  })
+  const { saveDraft } = useProductStore()
 
   return (
     <button
       className={`adminBorder w-16 mx-auto py-3 px-24 items-center justify-center flex`}
-      onClick={handleSave}
+      onClick={saveDraft}
     >
       SAVE
     </button>
   )
 }
-
-
-// TODO: config object passed into custom hook
