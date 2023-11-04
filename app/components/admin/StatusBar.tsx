347d23c82a398b6a81cb358057e499853717a534
waikoo <beroer@protonmail.com>
2023-11-04 23:09:58 +0200

    Display total nr of items in 'draft' table in db

--- app/components/admin/StatusBar.tsx
+++ app/components/admin/StatusBar.tsx
@@ -2,6 +2,8 @@
 import { useUIStore } from "@/state/uiState"
 import { useProductStore } from "@/state/productState"
 import { twMerge as tm } from 'tailwind-merge'
+import { useEffect, useState } from "react"
+import { createBrowserClient } from '@supabase/ssr'
 
 type StatusBarProps = {
   children: React.ReactNode
@@ -10,7 +12,25 @@ type StatusBarProps = {
 export default function StatusBar({ children }: StatusBarProps) {
   const { created, edited } = useProductStore((state) => state.counter)
   const { raiseStatusBar, statusBar } = useUIStore()
+  const [draftCount, setDraftCount] = useState(0)
   let height = !statusBar ? 'h-0' : 'h-[48vh]'
+
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+  useEffect(() => {
+    const fetchDrafts = async () => {
+
+      const { data, error } = await supabase
+        .from('draft')
+        .select('*');
+
+      setDraftCount(data?.length as number)
+    }
+    fetchDrafts()
+
+  }, [])
 
   return (
     <div className={tm(`bg-content ${height} text-bkg fixed bottom-0 left-0 right-0 grid w-screen text-[1.2rem] font-bold`)}
@@ -20,7 +40,7 @@ export default function StatusBar({ children }: StatusBarProps) {
       {children}
 
       <div className="bg-content fixed bottom-0 left-0 right-0 grid w-screen grid-cols-[auto_auto_1fr] gap-8 p-6 ">
-        <span>CREATED: {created} </span>
+        <span>CREATED: {draftCount} </span>
         <span className="justify-self-start">EDITED: {edited} </span>
 
         <button className="bg-bkg text-content cursor-pointer justify-self-end px-24 py-4 font-semibold tracking-wider">PUBLISH CHANGES</button>
