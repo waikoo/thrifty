9c9c96507d51ff8625b9a7677258ef65243b5bc9
waikoo <beroer@protonmail.com>
2023-11-30 18:04:34 +0200

    Add: SignIn disappears on click outside

--- app/components/navigation/CenterContainer.tsx
+++ app/components/navigation/CenterContainer.tsx
@@ -3,10 +3,12 @@ import { RecoverPassword, SignInOrUp } from ".";
 
 export default function CenterContainer() {
   const showRecovery = useUIStore((state) => state.showRecovery)
+  const { setShowSignIn } = useUIStore()
 
   return (
-    <dialog className="fixed inset-0 z-40 grid h-screen w-full place-items-center bg-black bg-opacity-50">
-
+    <dialog className="fixed inset-0 z-40 grid h-screen w-full place-items-center bg-black bg-opacity-50"
+      onClick={() => setShowSignIn(false)}
+    >
       <section className="bg-bkg relative p-8">
 
         {showRecovery ? <RecoverPassword /> : <SignInOrUp />}
