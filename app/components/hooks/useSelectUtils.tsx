0db378b4f08944fc1c3484773eb8a7f8a36921e8
waikoo <beroer@protonmail.com>
2023-11-30 20:05:08 +0200

    Refactor: replace switch statements with object lookup

--- app/components/hooks/useSelectUtils.tsx
+++ app/components/hooks/useSelectUtils.tsx
@@ -1,35 +1,42 @@
 import { useProductStore } from "@/state/productState"
 
+export type FieldName = 'gender' | 'category' | 'type' | 'color' | 'brand' | 'material' | 'condition';
+
 export default function useSelectUtils() {
   const { gender, category, type, color, brand, material, condition, setGender, setCategory, setType, setColor, setBrand, setMaterial, setCondition } = useProductStore()
 
-  const getValue = (lowerCaseName: string) => {
-    switch (lowerCaseName) {
-      case 'gender': return gender
-      case 'category': return category
-      case 'product type': return type
-      case 'color': return color
-      case 'brand': return brand
-      case 'material': return material
-      case 'condition': return condition
-      default: throw new Error('getValue in Select component: "name" does not match')
-    }
+  const values = {
+    gender,
+    category,
+    type,
+    color,
+    brand,
+    material,
+    condition
+  }
+  const getValue = (lowerCaseName: FieldName) => {
+    return values[lowerCaseName]
   }
 
-  const getOnChange = (e: React.ChangeEvent<HTMLSelectElement>, lowerCaseName: string) => {
-    const value = e.target.value
+  const setters: Record<FieldName, (value: string) => void> = {
+    gender: setGender,
+    category: setCategory,
+    type: setType,
+    color: setColor,
+    brand: setBrand,
+    material: setMaterial,
+    condition: setCondition,
+  };
 
-    switch (lowerCaseName) {
-      case 'gender': return setGender(value)
-      case 'category': return setCategory(value)
-      case 'product type': return setType(value)
-      case 'color': return setColor(value)
-      case 'brand': return setBrand(value)
-      case 'material': return setMaterial(value)
-      case 'condition': return setCondition(+value)
-      default: throw new Error('getOnChange in Select component: "name" does not match')
+  const getOnChange = (e: React.ChangeEvent<HTMLSelectElement>, lowerCaseName: FieldName) => {
+    const value = e.target.value;
+    const setter = setters[lowerCaseName];
+    if (!setter) {
+      throw new Error('getOnChange in Select component: "name" does not match');
     }
-  }
+    setter(value);
+  };
+
 
   return {
     getValue,
