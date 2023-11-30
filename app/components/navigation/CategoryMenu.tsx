652c887b7f291730f07692be8f6eeceb865ac005
waikoo <beroer@protonmail.com>
2023-11-30 18:27:21 +0200

    Style: Add dark & blurred bg and horizontal screen line

--- app/components/navigation/CategoryMenu.tsx
+++ app/components/navigation/CategoryMenu.tsx
@@ -17,20 +17,23 @@ export default function CategoryMenu() {
     }
   }
   return (
-    <div
-      className="bg-bkg border-content min-w-screen absolute inset-x-0 z-40 mx-auto flex gap-32 overflow-y-hidden border-t-[0.1rem] px-12 py-12"
-      onMouseLeave={(e) => handleMouseLeave(e as React.MouseEvent<HTMLDivElement, MouseEvent>)}
-      onMouseEnter={() => setShowCategoryMenu(true)}
-      ref={divRef}
-    >
-      {hoveredCategory === 'men' && <CategoryMen />}
-      {hoveredCategory === 'women' && <CategoryWomen />}
-      {hoveredCategory === 'kids' && <CategoryKids />}
+    <section className="absolute inset-0 top-10 h-screen bg-black bg-opacity-50 backdrop-blur-sm">
+      <div className="border-darkgrey absolute inset-x-0 left-[-15rem] z-50 w-screen border-t-[0.05rem]"></div>
+      <div
+        className="bg-bkg min-w-screen absolute inset-x-0 z-40 mx-auto flex gap-32 overflow-y-hidden px-12 py-12"
+        onMouseLeave={(e) => handleMouseLeave(e as React.MouseEvent<HTMLDivElement, MouseEvent>)}
+        onMouseEnter={() => setShowCategoryMenu(true)}
+        ref={divRef}
+      >
+        {hoveredCategory === 'men' && <CategoryMen />}
+        {hoveredCategory === 'women' && <CategoryWomen />}
+        {hoveredCategory === 'kids' && <CategoryKids />}
 
-      <div className="my-auto flex flex-col gap-10">
-        <CategoryImage>NEW IN</CategoryImage>
-        <CategoryImage>PROMOS</CategoryImage>
-      </div>
-    </div >
+        <div className="my-auto flex flex-col gap-10">
+          <CategoryImage>NEW IN</CategoryImage>
+          <CategoryImage>PROMOS</CategoryImage>
+        </div>
+      </div >
+    </section>
   )
 }
