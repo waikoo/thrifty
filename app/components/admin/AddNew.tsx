97acb14a50603ce596c143cf95ed599ab5ddb7c9
waikoo <beroer@protonmail.com>
2023-12-04 00:28:39 +0200

    Fix: popup not popping up

--- app/components/admin/AddNew.tsx
+++ app/components/admin/AddNew.tsx
@@ -7,24 +7,33 @@ type AddNewProps = {
 }
 
 export default function AddNew({ name, handleAddItem }: AddNewProps) {
-  const { addOption, showAddOption } = useUIStore()
+  const { addMaterial, showAddMaterial, addBrand, showAddBrand } = useUIStore()
+
+  const onClickHandler = () => {
+    if (name === 'BRAND') {
+      showAddBrand(true)
+    } else if (name === 'MATERIAL') {
+      showAddMaterial(true)
+    }
+  }
 
   return (
-    <div className="absolute right-[-7rem] ml-3 flex cursor-pointer items-center gap-2"
-      onClick={() => showAddOption(!addOption)}
-    >
-      <div className="h-[50%] self-center">
-        <span className="border-bkg bg-content text-bkg self-start border-[0.05rem] p-[0.005rem] px-[0.01rem] text-center text-[1.5rem]">+</span>
-      </div>
-      <span className="text-grey">Add new</span>
+    <>
+      <div className="absolute right-[-7rem] ml-3 flex cursor-pointer items-center gap-2"
+        onClick={onClickHandler}
+      >
+        <div className="h-[50%] self-center">
+          <span className="border-bkg bg-content text-bkg self-start border-[0.05rem] p-[0.005rem] px-[0.01rem] text-center text-[1.5rem]">+</span>
+        </div>
+        <span className="text-grey">Add new</span>
 
-      {addOption ? (
+      </div>
+
+      {(name === 'BRAND' && addBrand) || (name === 'MATERIAL' && addMaterial) ? (
         <Portal>
           <AddOptionPopUp {...{ name, handleAddItem }} />
         </Portal>
       ) : null}
-
-    </div>
-
+    </>
   )
 }
--- app/components/admin/AddOptionPopUp.tsx
+++ app/components/admin/AddOptionPopUp.tsx
@@ -9,15 +9,43 @@ type AddOptionPopUpProps = {
 
 export default function AddOptionPopUp({ name, handleAddItem }: AddOptionPopUpProps) {
   const inputRef = useRef<null | HTMLInputElement>(null)
-  const { addOption, showAddOption } = useUIStore()
-  console.log(name)
+  const { addMaterial, showAddMaterial, addBrand, showAddBrand } = useUIStore()
+  const outerDiv = useRef<null | HTMLDivElement>(null)
+  const buttonRef = useRef<null | HTMLButtonElement>(null)
+  const xDiv = useRef<null | HTMLDivElement>(null)
+
+  const onClickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.currentTarget.localName === 'div') {
+      closePopup(e)
+    }
+  }
+
+  const closePopup = (e: React.MouseEvent<HTMLDivElement | HTMLButtonElement>) => {
+    if ([outerDiv.current, buttonRef.current, xDiv.current].some(el => el === e.currentTarget)) {
+      if (name === 'BRAND') {
+        showAddBrand(!addBrand)
+      } else if (name === 'MATERIAL') {
+        showAddMaterial(!addMaterial)
+      }
+    }
+  }
+
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    inputRef.current?.value && handleAddItem!(inputRef.current.value);
+    inputRef.current!.value = ''
+
+    closePopup(e)
+  }
 
   return (
     <div
-      className="text-bkg absolute inset-0 z-[60] grid h-screen w-screen place-items-center bg-[rgba(0,0,0,0.1)] shadow">
-      <div className="bg-content text-bkg w-min-content flex flex-col items-center justify-center p-3">
+      className="text-bkg absolute inset-0 z-[60] grid h-screen w-screen place-items-center bg-[rgba(0,0,0,0.1)] shadow"
+    >
+      <div className="bg-content text-bkg w-min-content flex flex-col items-center justify-center p-3"
+      >
 
-        <div className="w-full text-right" onClick={() => showAddOption(false)}>
+        <div className="w-full text-right" onClick={onClickHandler} ref={xDiv}>
           <span className="cursor-pointer">X</span>
         </div>
 
@@ -27,13 +55,10 @@ export default function AddOptionPopUp({ name, handleAddItem }: AddOptionPopUpPr
         <input ref={inputRef} type="text" className="text-bkg border-bkg w-[80%] border-[0.1rem] p-[0.5rem]" name="add-option" id="add-option" />
 
         <button
-          onClick={(e) => {
-            e.preventDefault();
-            inputRef.current?.value && handleAddItem!(inputRef.current.value);
-            inputRef.current!.value = ''
-            showAddOption(false)
-          }}
-          className="text-content bg-bkg mt-6 block px-10 py-3">
+          onClick={onClick}
+          className="text-content bg-bkg mt-6 block px-10 py-3"
+          ref={buttonRef}
+        >
           SAVE
         </button>
       </div>
