3863228daaff782cef37a0e60f590795fd51e789
waikoo <beroer@protonmail.com>
2023-12-03 21:41:12 +0200

    Refactor: use server-side supabase client

--- app/components/admin/StatusImages.tsx
+++ app/components/admin/StatusImages.tsx
@@ -1,28 +1,13 @@
 import { fetchAllProducts } from "@/utils/fetchAllProducts";
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { cookies } from 'next/headers'
+import { useSupabaseServer } from "@/app/components/hooks/serverIndex"
 import Image from 'next/image'
 
 export default async function StatusImages() {
-  const cookieStore = cookies()
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        }
-      }
-    }
-  )
-
-  const data = await fetchAllProducts(supabase, 'draft')
+  const data = await fetchAllProducts(useSupabaseServer(), 'draft')
 
   return (
     <div className="flex h-[26.8rem] p-6">
-      <div className="hover:bg-darkgrey flex h-full max-w-[50%] flex-wrap gap-[0.5rem] overflow-y-scroll rounded-lg p-6">
+      <div className="hover:bg-darkgrey flex h-full w-[50%] max-w-[50%] flex-wrap gap-[0.5rem] overflow-y-scroll rounded-lg p-6">
         {data?.filter(el => el.img_url && el.img_url.length > 0)
           .map((el, i) => (
             <div className="w-[19.1%] flex-grow-0 object-cover"
@@ -37,7 +22,6 @@ export default async function StatusImages() {
             </div>
           ))}
       </div>
-
 
       <div className="max-w-[50%]">right</div>
     </div>
