7dbbf9214a10585c7cfc75f5369579cc46c2196f
waikoo <beroer@protonmail.com>
2023-12-08 22:42:56 +0200

    Refactor: rearrange layouts to match design

--- app/[lang]/(admin)/admin/manage/page.tsx
+++ app/[lang]/(admin)/admin/manage/page.tsx
@@ -1,11 +1,36 @@
 import { Manage } from "@/app/components/admin";
 
-export default function Page() {
+import { Banner, NavBar } from "@/app/components/navigation";
+import { themeSettings } from '@/app/components/data';
+import "@/styles/styles.css";
+import { LayoutAddNew, LayoutMenu, LayoutSearchId, StatusBar } from '@/app/components/admin';
+import { StatusImages } from '@/app/components/admin/indexServer';
+
+type PageProps = {
+  params: { [key: string]: string | string[] | undefined }
+}
+
+export default function Page({ params }: PageProps) {
 
   return (
     <>
-      <Manage />
-      <div id="popup-root"></div>
+      <section className="bg-bkg text-content mx-auto flex h-screen max-w-[1600px] flex-col items-center px-20 lg:max-w-[1500px]">
+        <NavBar isAdmin params={params} />
+
+        <main className="mt-6 flex w-full flex-col items-center">
+          <Manage />
+          <div id="popup-root"></div>
+        </main>
+
+      </section>
+
+      <footer>
+        <StatusBar>
+          <StatusImages />
+        </StatusBar>
+
+      </footer>
     </>
   )
 }
+
--- app/[lang]/(admin)/layout.tsx
+++ app/[lang]/(admin)/layout.tsx
@@ -22,38 +22,10 @@ export default function RootLayout({ children, params }: RootLayoutProps) {
     <html data-theme={themeSettings.DEFAULT_THEME} className={inter.className}>
       <head className={inter.className} />
       <body className={`bg-bkg ${inter.className}`}>
-        <Banner />
-        <section className="bg-bkg text-content mx-auto flex h-[80vh] max-w-[1600px] flex-col items-center px-20 lg:max-w-[1500px]">
-
-          <nav className="flex w-full items-center justify-between pt-6">
-            <div className="w-[50%]">
-              <LayoutAddNew params={params} />
-            </div>
-
-            <div className="flex w-[50%] items-center">
-              <LayoutSearchId />
-
-              <div className="ml-auto flex items-center gap-2">
-                <IconAccount />
-                <span>ADMIN</span>
-              </div>
-
-            </div>
-          </nav>
-
-          <main className="flex w-full">
-            {children}
-          </main>
-
-        </section>
-
-        <footer className="mx-auto grid max-w-[1600px] grid-cols-3 items-center px-20 lg:max-w-[1500px]">
-          <span className="text-bold text-content underline underline-offset-2">SELECT</span>
-          <Logo />
-          <PublishChanges className="border-content border-2" />
-        </footer>
+      <Banner />
+      {children}
       </body>
-    </html>
+    </html >
   )
 }
 
