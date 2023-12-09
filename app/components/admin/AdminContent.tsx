8051d4d9cb7e82d27103cbab430e687f73ef6366
waikoo <beroer@protonmail.com>
2023-12-09 21:20:17 +0200

    Add: Log single product details on hover

--- app/components/admin/AdminContent.tsx
+++ app/components/admin/AdminContent.tsx
@@ -1,9 +1,29 @@
 import { ImEnlarge } from "react-icons/im";
 import Image from 'next/image'
 import { useDraftTable } from "../hooks";
+import { useState } from "react";
 
 export default function AdminContent() {
   const draft = useDraftTable()
+  const [showPopup, setShowPopup] = useState(new Array(draft.length).fill(false))
+
+  console.table(draft)
+
+  const onMouseOver = (i: number) => {
+    setShowPopup(prevState => {
+      const newState = [...prevState];
+      newState[i] = true;
+      return newState;
+    });
+  }
+
+  const onMouseLeave = (i: number) => {
+    setShowPopup(prevState => {
+      const newState = [...prevState];
+      newState[i] = false;
+      return newState;
+    });
+  }
 
   return (<>
     {/* <div id="popup-root"></div> */}
@@ -11,11 +31,24 @@ export default function AdminContent() {
 
       <div className="bg-content relative h-[70vh] w-[50%]">
 
-
         <div className="flex h-full w-full flex-wrap gap-[0.5rem] overflow-y-scroll rounded-lg p-6">
           {draft?.filter(el => el.img_url && el.img_url.length > 0).map((el, i) => (
             <div className="w-[19.1%] flex-grow-0 object-cover"
               key={i}
+              data-uuid={el.uuid}
+              data-created-at={el.created_at}
+              data-gender={el.gender}
+              data-category={el.category}
+              data-price={el.price}
+              data-discount={el.discount}
+              data-size={el.size}
+              data-color={el.color}
+              data-brand={el.brand}
+              data-condition={el.condition}
+              data-material={el.material}
+              data-images-length={el.img_url.length}
+              onMouseOver={() => onMouseOver(i)}
+              onMouseLeave={() => onMouseLeave(i)}
             >
               <Image
                 className="block min-w-full"
@@ -23,6 +56,14 @@ export default function AdminContent() {
                 alt={`new-product-${i}`}
                 width={100}
                 height={100} />
+              {showPopup[i] &&
+                <section >
+                  <div className="flex gap-2">
+                    <span>ID</span>
+                    <span>{el.uuid}</span>
+                  </div>
+                </section>
+              }
             </div>
           ))}
         </div>
