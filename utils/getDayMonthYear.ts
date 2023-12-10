a0c3fabfd81e830c24ae9c3dd660003cf1758735
waikoo <beroer@protonmail.com>
2023-12-10 20:53:02 +0200

    Fix: arg of wrong type

--- utils/getDayMonthYear.ts
+++ utils/getDayMonthYear.ts
@@ -1,4 +1,4 @@
-export function getDayMonthYear(thedate?: Date) {
+export function getDayMonthYear(thedate?: string) {
   const date = typeof thedate === 'string' ? new Date(thedate) : new Date();
   const month = date.getMonth() + 1;
   const day = date.getDate();
