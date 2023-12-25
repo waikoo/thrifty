747a6836930f5c07d35988382fb97e66bb62ec67
waikoo <beroer@protonmail.com>
2023-12-25 17:56:50 +0200

    Change: product type access based on gender

--- app/components/data/filterArrays.ts
+++ app/components/data/filterArrays.ts
@@ -1,7 +1,21 @@
 export const filter = {
   category: ["Men", "Women", "Kids"],
   shopBy: ["New In", "Promos"],
-  productType: ["Coats", "Jackets", "Tops", "T-Shirts", "Sweaters", "Jeans", "Pants"],
+  productType: {
+    men: ["Clothing", "Coats", "Jackets", "Tops", "T-Shirts", "Sweaters", "Jeans", "Pants",
+      "Shoes", "Trainers", "Boots", "Elegant", "Sandals", "Slippers",
+      "Accessories", "Bags", "Sunglasses", "Watches", "Hats", "Belts",
+      "Sport", "Trainers", "Joggers", "Hoodies", "Tops"]
+    ,
+    women: ["Clothing", "Jackets", "Coats", "Dresses", "Tops", "T-Shirts", "Jeans", "Pants", "Skirts", "Dresses",
+      "Shoes", "Trainers", "Boots", "Heels", "Flats", "Sandals", "Slippers",
+      "Accessories", "Bags", "Sunglasses", "Jewellery", "Watches", "Hats", "Belts",
+      "Sport", "Trainers", "Joggers", "Hoodies", "Tops"]
+    ,
+    kids: ["Clothing", "Coats", "Jackets", "Tops", "T-Shirts", "Sweaters", "Jeans", "Pants", "Skirts", "Dresses",
+      "Shoes", "Trainers", "Boots", "Flats", "Sandals", "Slippers",
+      "Sport", "Trainers", "Joggers", "Hoodies", "Tops"]
+  },
   size: ["35", "35.5", "36", "36.5", "37", "37.5", "38", "38.5", "39", "39.5", "40", "40.5", "41", "41.5", "42", "42.5", "43", "43.5", "44", "44.5", "46", "XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL"],
   color: ["Black", "White", "Beige", "Brown", "Grey", "Blue", "Purple", "Multicolor", "Pink", "Red", "Green", "Yellow", "Orange", "Gold", "Silver"],
   condition: [5, 4, 3],
