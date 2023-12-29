98ae8f7a7b212ada2a4ec9c9b355fbad0949b123
waikoo <beroer@protonmail.com>
2023-12-29 21:59:31 +0200

    Remove: logic, make component dumber

--- app/components/admin/StatusBar.tsx
+++ app/components/admin/StatusBar.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useUIStore } from "@/state/uiState"
 import { twMerge as tm } from 'tailwind-merge'
-import { useTable } from "../hooks"
 import { PublishChanges } from "."
 
 type StatusBarProps = {
@@ -9,10 +8,8 @@ type StatusBarProps = {
 }
 
 export default function StatusBar({ children }: StatusBarProps) {
-  const { raiseStatusBar, statusBar, isSaved, setIsSaved } = useUIStore()
+  const { raiseStatusBar, statusBar, editedLength, draftLength } = useUIStore()
   let height = !statusBar ? 'h-0' : 'h-[48vh]'
-  const draft = useTable('draft')
-  const edited = useTable('edited')
 
   return (
     <div className={tm(`bg-content ${height} text-bkg fixed bottom-0 left-0 right-0 grid w-screen font-bold`)}
@@ -23,8 +20,8 @@ export default function StatusBar({ children }: StatusBarProps) {
 
       <div className="bg-content fixed bottom-0 left-0 right-0 w-screen p-6 ">
         <div className={`mx-auto grid max-w-[1700px] ${!statusBar ? 'grid-cols-[auto_auto_1fr]' : 'grid-cols-[48%_auto_1fr]'} items-baseline gap-8`}>
-          <span>NEW: {draft.length} </span>
-          <span className="justify-self-start">EDITED: {edited?.length} </span>
+          <span>NEW: {draftLength} </span>
+          <span className="justify-self-start">EDITED: {editedLength} </span>
 
           <PublishChanges className="border-bkg border-2" />
         </div>
