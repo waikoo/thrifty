677546777b45e49eead89ce0ef2977ef915ad060
waikoo <beroer@protonmail.com>
2024-01-10 23:50:28 +0200

    Style: missing section in Returns Info

--- app/components/products/ProductShippingReturnsInfo.tsx
+++ app/components/products/ProductShippingReturnsInfo.tsx
@@ -13,7 +13,7 @@ export default function ProductShippingReturnsInfo() {
         <span className={`cursor-pointer ${returnsStyle}`} onClick={() => setInfo('returns')}>RETURNS INFO</span>
       </div>
 
-      <div className="bg-faded text-bkg mt-5 p-5">
+      <div className="bg-faded text-bkg mt-5 h-[7.8rem] p-5">
         {info === 'shipping' ? (
           <div className="flex flex-col gap-3">
             <p className="flex gap-1 text-[0.625rem] font-medium">
@@ -32,11 +32,12 @@ export default function ProductShippingReturnsInfo() {
             </p>
           </div>
         ) : (
-          <div>
-            <span>FREE<span>30-DAY RETURN POLICY</span></span>
-          </div>
+          <p className="flex gap-1 text-[0.625rem] font-medium">
+            <strong className="font-bold">FREE</strong>
+            <span>30-DAY RETURN POLICY</span>
+          </p>
         )}
-        <span className="font-regular block text-right text-[0.6875rem] underline underline-offset-4">More Info</span>
+        <span className="font-regular block cursor-pointer text-right text-[0.6875rem] underline underline-offset-4">More Info</span>
       </div>
 
     </div>
