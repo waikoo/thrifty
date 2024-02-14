7bdc413a804b2a9793060187c9de79129366d4f2
waikoo <beroer@protonmail.com>
2024-02-14 20:01:37 +0200

    Refactor: use array access with variable instead of double computation

--- utils/getLangAndGender.ts
+++ utils/getLangAndGender.ts
@@ -1,8 +1,10 @@
 import { Category, Locales } from "@/types/home"
 
 export default function getLangAndGender(pathname: string) {
+  const pathnameArr = pathname.split('/')
+
   return {
-    lang: pathname.split('/')[1] as Locales,
-    gender: pathname.split('/')[2] as Category['category']
+    lang: pathnameArr[1] as Locales,
+    gender: pathnameArr[2] as Category['category'],
   }
 }
