1c2f7c469748562cb75f46efc308cb99d8477ec2
waikoo <beroer@protonmail.com>
2024-02-21 20:46:17 +0200

    Style: /returns

--- app/[lang]/(client)/[gender]/returns/page.tsx
+++ app/[lang]/(client)/[gender]/returns/page.tsx
@@ -1,17 +1,7 @@
-import { Category, Locales } from "@/types/home"
-
-type PageProps = {
-  params: {
-    lang: Locales,
-    gender: Category['category']
-  }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
-export default function Page({ params }: PageProps) {
+export default function Page() {
 
   return (
-    <div className="text-content">Returns Page</div>
+    <div className="text-content my-20 text-center">No returns were found.</div>
   )
 }
 
