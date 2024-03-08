10c84f8ad2eeb670f337ad360cb9922b1bb5c23b
waikoo <beroer@protonmail.com>
2024-03-08 23:18:30 +0200

    Add: links to FooterAccount

--- app/components/footer/FooterAccount.tsx
+++ app/components/footer/FooterAccount.tsx
@@ -1,13 +1,27 @@
+"use client"
+import getLangAndGender from "@/utils/getLangAndGender";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useUIStore } from "@/state"
+import { useUserSession } from "@/app/components/hooks"
 
 export default function FooterAccount() {
+  const { gender, lang } = getLangAndGender(usePathname())
+  const { showMyAccount, setShowMyAccount, showSignIn, setShowSignIn } = useUIStore()
+  const { session, error } = useUserSession()
+
   return (
     <div className="footerText flex flex-col gap-7">
       <h3 className="footerTitles">ACCOUNT</h3>
       <div className="flex flex-col gap-2">
-        <Link href="/en/login">Sign In/Up</Link>
-        <Link href="/en/register">My Profile</Link>
-        <Link href="/en/settings">Settings</Link>
+        <span className="cursor-pointer"
+          onClick={() => !session
+            ? setShowSignIn(showSignIn ? false : true)
+            : setShowMyAccount(showMyAccount ? false : true)}
+        >Sign In/Up</span>
+
+        <Link href={`/${lang}/${gender}/profile`}>My Profile</Link>
+        <Link href={`/${lang}/${gender}/settings`}>Settings</Link>
       </div>
     </div>
   )
