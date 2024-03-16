9b4d58c875d46d12717f58fcc489fcc7c9a9c35e
waikoo <beroer@protonmail.com>
2024-03-16 17:20:52 +0200

    Style: add sticky NavBar

--- app/components/navigation/NavBar.tsx
+++ app/components/navigation/NavBar.tsx
@@ -1,15 +1,17 @@
 "use client"
+import { useEffect, useState } from "react"
+
 import { useUIStore } from "@/state"
 import { useDarkMode } from "@/app/components/hooks"
-import { Logo, SearchBar, WithHome } from '@/app/components/navigation/'
+import { Category, Logo, SearchBar, WithHome } from '@/app/components/navigation/'
 import NavIcons from "@/app/components/navigation/NavIcons"
+import { usePathname } from "next/navigation"
 
 type NavBarProps = {
-  isAdmin?: boolean
   params?: { [key: string]: string | string[] | undefined }
 }
 
-const NavBar = ({ isAdmin, params }: NavBarProps) => {
+const NavBar = ({ params }: NavBarProps) => {
 
   const htmlDataset = typeof document !== 'undefined' ? document.documentElement.dataset : undefined
   if (htmlDataset) {
@@ -17,16 +19,37 @@ const NavBar = ({ isAdmin, params }: NavBarProps) => {
   }
 
   const { setShowCategoryMenu } = useUIStore()
+  const [position, setPosition] = useState('static')
+  const noBorderOnScroll = position === 'static' ? 'border-content border-b-2' : ''
+  const endpoint = usePathname().split('/')[3]
+
+  const handleScroll = () => {
+    if (endpoint !== 'products') {
+      setPosition(window.scrollY > 5 ? 'fixed' : 'static')
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
   return (
-    <section className="w-full max-w-[1440px] ">
-      <div className={`border-content relative mx-auto grid w-full grid-cols-3 border-b-2 pb-2 pt-4`}
+    <section className={`bg-bkg ${position} top-0 z-50 w-screen`}>
+      <div className={`${noBorderOnScroll} relative mx-auto grid w-full max-w-[1440px] grid-cols-3 pb-2 pt-4`}
         onMouseEnter={() => setShowCategoryMenu(false)} // makes categorymenu disappear when exiting with mouseover on top
       >
         <SearchBar className="self-end" />
 
-        <WithHome> <Logo className="self-end" /> </WithHome>
-
+        {position === 'static' ?
+          (
+            <WithHome> <Logo className="self-end" /> </WithHome>
+          ) : (
+            <Category />
+          )
+        }
+        {/* on scroll Category shows up instead of Thriftstudio logo */}
         <NavIcons className="self-end justify-self-end" />
       </div>
     </section>
