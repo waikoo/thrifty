136a8ac55d539726dd3c8533cf0ecdd5bb9663e2
waikoo <beroer@protonmail.com>
2024-03-20 20:53:05 +0200

    Style: AnimatedInput value

--- app/components/AnimatedInput.tsx
+++ app/components/AnimatedInput.tsx
@@ -15,15 +15,13 @@ export default function AnimatedInput({ type, id, placeholder, value, onChange, 
   return (
     <div className="relative">
       <input
-        className={tm(`${className} bg-bkg text-grey border-b-grey border-grey peer w-full border-x-0 border-b-[0.1rem] border-t-0 border-solid pl-0 placeholder-transparent focus:border-sky-500 focus:outline-none focus:ring-0`)}
+        className={tm(`${className} bg-bkg text-content border-b-grey border-grey peer w-full border-x-0 border-b-[0.1rem] border-t-0 border-solid pl-0 placeholder-transparent focus:border-sky-500 focus:outline-none focus:ring-0`)}
         id={id}
         type={type}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
         spellCheck="false"
-        // onFocus={handleOnFocus}
-        // onBlur={handleOnBlur}
         required
       />
       <label
