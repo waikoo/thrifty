419bb2e38960aeb33bc341346b089e8054ebcada
waikoo <beroer@protonmail.com>
2024-03-28 18:33:56 +0200

    Fix: SignInOrUp vanished on click

--- app/components/navigation/Account.tsx
+++ app/components/navigation/Account.tsx
@@ -1,6 +1,4 @@
 "use client"
-import { useEffect, useRef } from "react"
-
 import { useUserSession } from "@/app/components/hooks"
 import { useUIStore } from "@/state"
 import { AccountMenu, CenterContainer } from "@/app/components/navigation"
@@ -10,25 +8,9 @@ import { IconAccount } from '@/app/components/navigation/icons/'
 export default function Account() {
   const { session, error } = useUserSession()
   const { showMyAccount, setShowMyAccount, showSignIn, setShowSignIn } = useUIStore()
-  const divRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const handleOutsideClick = (e: MouseEvent) => {
-      if (divRef.current && !divRef.current.contains(e.target as Node)) {
-        setShowMyAccount(false);
-        setShowSignIn(false);
-      }
-    };
-
-    window.addEventListener('mousedown', handleOutsideClick);
-
-    return () => {
-      window.removeEventListener('mousedown', handleOutsideClick);
-    };
-  }, []);
 
   return (
-    <div className="relative" ref={divRef}>
+    <div className="relative">
       <div
         title={session?.user.role ? "Account" : "Sign In"}
         onClick={() => !session
--- app/components/navigation/CenterContainer.tsx
+++ app/components/navigation/CenterContainer.tsx
@@ -1,13 +1,24 @@
+"use client"
+import { useRef } from "react";
+
 import { useUIStore } from "@/state";
 import { RecoverPassword, SignInOrUp } from ".";
 
 export default function CenterContainer() {
   const showRecovery = useUIStore((state) => state.showRecovery)
   const { setShowSignIn } = useUIStore()
+  const dialogRef = useRef(null)
+
+  const handleOutsideClick = (e: React.MouseEvent) => {
+    if (e.target === dialogRef.current) {
+      setShowSignIn(false)
+    }
+  }
 
   return (
     <dialog className="fixed inset-0 z-40 grid h-screen w-full place-items-center bg-black bg-opacity-50"
-      onClick={() => setShowSignIn(false)}
+      onClick={handleOutsideClick}
+      ref={dialogRef}
     >
       <section className="bg-bkg relative p-8">
 
