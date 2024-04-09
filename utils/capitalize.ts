14de4b70fcb7c880d31d4403c2ad1a14bcea18ec
waikoo <beroer@protonmail.com>
2024-04-09 19:57:11 +0300

    Refactor: use a helper function in capitalize()

--- utils/capitalize.ts
+++ utils/capitalize.ts
@@ -2,8 +2,12 @@ export const capitalize = (str: string) => {
   if (str.split(' ').length > 1) {
     return str
       .split(' ')
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .map(word => capitalizeAWord(word))
       .join(' ')
   }
+  return capitalizeAWord(str)
+}
+
+const capitalizeAWord = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
