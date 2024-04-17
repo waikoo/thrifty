c43c92d03863449cd7f7f4afe4e2128bd736cd50
waikoo <beroer@protonmail.com>
2024-04-17 22:17:40 +0300

    Add: messages

--- app/components/data/bannerMessages.ts
+++ app/components/data/bannerMessages.ts
@@ -2,4 +2,6 @@ import { EURO } from "@/app/components/data/orderSummary";
 
 export const bannerMessages = [
   `Free shipping above ${EURO}15`,
+  'Product reservation valid until end of day',
+  'New arrivals every Monday'
 ];
