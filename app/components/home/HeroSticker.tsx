e6bbc84b46cb462d3c7306bea7a89344b52e20f7
waikoo <beroer@protonmail.com>
2024-04-20 18:59:48 +0300

    Remove: unused variables, imports

--- app/components/home/HeroSticker.tsx
+++ app/components/home/HeroSticker.tsx
@@ -1,20 +1,14 @@
 "use client"
 import { useHomeStore } from '@/state/client/homeState';
-import { HeroState } from '@/types/home';
-import getLangAndGender from '@/utils/getLangAndGender';
 import { getText } from '@/utils/home';
-import { usePathname } from 'next/navigation';
 
 export default function HeroSticker() {
   const { heroState } = useHomeStore()
-  const { gender } = getLangAndGender(usePathname())
+
   const newInOrSale = {
     bg: heroState === 'new_in' ? 'bg-t_yellow' : 'bg-t_red',
     text: heroState === 'new_in' ? 'text-t_black' : 'text-t_yellow',
   }
-  const onWomenNewIn = heroState === 'new_in' && gender === 'women' ? '' : ''
-  const onMenNewIn = heroState === 'new_in' && gender === 'women' ? '' : ''
-  const onKidsNewIn = heroState === 'new_in' && gender === 'women' ? '' : ''
 
   return (
     <div className={`absolute top-14 left-6 ${newInOrSale.bg} ${newInOrSale.text} w-[86px] h-[86px] grid place-items-center rounded-full md:top-5 lg:w-[180px] lg:h-[180px] lg:top-10 lg:left-10`}>
