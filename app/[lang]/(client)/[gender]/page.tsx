de82c88eae8d9d6650be79fd24b9feba453d871a
waikoo <beroer@protonmail.com>
2024-04-20 18:56:45 +0300

    Style: remove flex

--- app/[lang]/(client)/[gender]/page.tsx
+++ app/[lang]/(client)/[gender]/page.tsx
@@ -1,5 +1,4 @@
 import { ApparelGrid, PopularBrands, NewsletterSubscription } from "@/app/components/home"
-import { NewArrivals } from "@/app/components/home/serverIndex"
 import Hero from "@/app/components/home/Hero"
 import { Gender, Locales } from "@/types/link"
 import HeroBarSwitch from "@/app/components/home/HeroBarSwitch"
@@ -15,13 +14,12 @@ type PageProps = {
 export default async function Page({ params: { lang, gender }, searchParams, }: PageProps) {
 
   return (
-    <main className="bg-bkg text-content mx-auto mt-2 flex w-full flex-col overflow-x-hidden overflow-y-hidden items-center xl:max-w-[1280px] max-w-[90vw]">
+    <main className="mx-auto mt-2 w-full overflow-hidden max-w-[90vw] xl:max-w-[1280px]">
       <Hero />
       <HeroBarSwitch />
 
       <ColorCarousel gender={gender} />
 
-      {/* <NewArrivals {... { lang, gender }} /> */}
       {/* <ApparelGrid {...{ lang }} /> */}
       {/* <PopularBrands {... { lang, gender }} /> */}
       {/* <NewsletterSubscription {... { lang }} /> */}
