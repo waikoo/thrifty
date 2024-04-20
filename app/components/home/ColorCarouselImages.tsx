d6872fb4e9213069afc4f6e181eb6f7a44ba24ce
waikoo <beroer@protonmail.com>
2024-04-20 16:15:50 +0300

    Add: clickable ColorCarousel

--- app/components/home/ColorCarouselImages.tsx
+++ app/components/home/ColorCarouselImages.tsx
@@ -1,5 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
+"use client"
 import { Gender } from "@/types/link"
+import { RiArrowDropRightFill } from "react-icons/ri"
+import { RiArrowDropLeftFill } from "react-icons/ri"
+import { useState } from "react";
+import { pluralToSingular, removeExtension } from "@/utils/home";
 
 type ColorCarouselImagesProps = {
   images: string[]
@@ -7,19 +12,49 @@ type ColorCarouselImagesProps = {
 }
 
 export default function ColorCarouselImages({ images, gender }: ColorCarouselImagesProps) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    const newIndex = (currentIndex + 1) % images.length;
+    setCurrentIndex(newIndex);
+  };
+
+  const prevSlide = () => {
+    const newIndex = (currentIndex - 1 + images.length) % images.length;
+    setCurrentIndex(newIndex);
+  };
+
+  const rightInvisible = currentIndex < 6 ? '' : 'invisible'
+  const leftInvisible = currentIndex > 0 ? '' : 'invisible'
 
   return (
-    <div className="grid grid-cols-2 gap-[0.5rem] md:grid-cols-3 md:gap-[1rem] lg:gap-[2rem] lg:grid-cols-4">
-      {images.map(filename => (
-        <img
-          src={`/images/color_carousel/${gender}/${filename}`}
-          alt={filename}
-          key={filename}
-          className="rounded-[10rem]"
-          width={100}
-          height={100}
-        />
-      ))}
+    <div className="mx-auto relative px-5">
+      <div className="flex gap-[1rem] overflow-hidden snap-x snap-mandatory"
+      >
+        {images.map((filename, i) => (
+          <div
+            key={filename}
+            className={`w-[30rem] ${i === currentIndex || i === currentIndex + 1 ? '' : 'hidden'}`}
+          >
+            <img
+              src={`/images/color_carousel/${gender}/${filename}`}
+              alt={`a ${pluralToSingular(gender)} in ${removeExtension(filename)} clothes`}
+              className="rounded-[10rem] w-[10rem] w-min-full"
+            />
+          </div>
+        ))}
+      </div>
+      <div className="absolute top-1/2 -translate-y-1/2 -left-5 -right-5 flex justify-between">
+        {/* {currentIndex > 0 && */}
+        <div onClick={prevSlide} className={leftInvisible}>
+          <RiArrowDropLeftFill size={60} color="black" />
+        </div>
+        {/* } */}
+        <div onClick={nextSlide} className={rightInvisible}>
+          <RiArrowDropRightFill size={60} color="black" />
+        </div>
+
+      </div>
     </div>
   )
 }
--- utils/home.ts
+++ utils/home.ts
@@ -5,3 +5,13 @@ export const getText = (text: string) => {
   }
   return currentText.toUpperCase()
 }
+
+export const removeExtension = (filename: string) => {
+  return filename.replace(/\.[^/.]+$/, "")
+}
+
+export const pluralToSingular = (text: string) => {
+  if (text === 'women') return 'woman'
+  if (text === 'men') return 'man'
+  if (text === 'kids') return 'kid'
+}
