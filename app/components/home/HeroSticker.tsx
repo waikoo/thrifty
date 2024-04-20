8dfb617caab316974fd92fbb025372c21f74cd74
waikoo <beroer@protonmail.com>
2024-04-20 19:59:58 +0300

    Style: tighter text in HeroSticker

--- app/components/home/HeroSticker.tsx
+++ app/components/home/HeroSticker.tsx
@@ -12,7 +12,7 @@ export default function HeroSticker() {
 
   return (
     <div className={`absolute top-14 left-6 ${newInOrSale.bg} ${newInOrSale.text} w-[86px] h-[86px] grid place-items-center rounded-full md:top-5 lg:w-[180px] lg:h-[180px] lg:top-10 lg:left-10`}>
-      <span className="text-[1.3rem] font-bold font-futura tracking-[1px] lg:text-[3.4375rem]">{getText(heroState)}</span>
+      <span className="text-[1.3rem] font-bold font-futura tracking-tight lg:text-[3.4375rem]">{getText(heroState)}</span>
     </div>
   )
 }
