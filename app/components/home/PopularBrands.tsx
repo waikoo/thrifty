cf656bef36735c84b2f5cf71d36a6e56a8c6facd
waikoo <beroer@protonmail.com>
2024-04-21 23:39:37 +0300

    Style: cut off parts works in light & dark mode too

--- app/components/home/PopularBrands.tsx
+++ app/components/home/PopularBrands.tsx
@@ -5,10 +5,11 @@ import PopularBrandsImages from '@/app/components/home/PopularBrandsImages'
 export default function PopularBrands() {
   const { theme } = useThemeStore()
   const textColor = theme === 'dark' ? 'text-t_black' : 'text-t_white'
+  const bgColor = theme === 'dark' ? 'bg-t_black' : 'bg-t_white'
 
   return (
     <section className={`${textColor} bg-t_mustard w-full max-w-full relative`}>
-      <div className="absolute top-0 left-0 right-0 z-10 bg-t_white h-10 -mt-2"></div>
+      <div className={`absolute top-0 left-0 right-0 z-10 ${bgColor} h-10 -mt-2`}></div>
       <span className={`text-[13.8vw] font-alokary block text-center`}>
         POPULAR
       </span>
@@ -18,7 +19,7 @@ export default function PopularBrands() {
       <span className="text-[15.5vw] font-alokary block text-center">
         BRANDS
       </span>
-      <div className="absolute bottom-0 left-0 right-0 z-10 bg-t_white h-6 -mb-2"></div>
+      <div className={`absolute bottom-0 left-0 right-0 z-10 ${bgColor} h-6 -mb-2`}></div>
     </section>
   )
 }
