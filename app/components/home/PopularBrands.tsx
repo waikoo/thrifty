b38c1763e308b7daaf2fb036e435e4779b62d789
waikoo <beroer@protonmail.com>
2024-04-21 23:37:34 +0300

    Style: cut off top and bottom part of PopularBrands

--- app/components/home/PopularBrands.tsx
+++ app/components/home/PopularBrands.tsx
@@ -7,8 +7,8 @@ export default function PopularBrands() {
   const textColor = theme === 'dark' ? 'text-t_black' : 'text-t_white'
 
   return (
-    <section className={`${textColor} bg-t_mustard w-full max-w-full`}>
-
+    <section className={`${textColor} bg-t_mustard w-full max-w-full relative`}>
+      <div className="absolute top-0 left-0 right-0 z-10 bg-t_white h-10 -mt-2"></div>
       <span className={`text-[13.8vw] font-alokary block text-center`}>
         POPULAR
       </span>
@@ -18,6 +18,7 @@ export default function PopularBrands() {
       <span className="text-[15.5vw] font-alokary block text-center">
         BRANDS
       </span>
+      <div className="absolute bottom-0 left-0 right-0 z-10 bg-t_white h-6 -mb-2"></div>
     </section>
   )
 }
