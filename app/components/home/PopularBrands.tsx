ce3d9fb72846c658ed20d2bb104df42ff9e84f46
waikoo <beroer@protonmail.com>
2024-04-22 19:07:45 +0300

    Style: adjust Brands text upper margin & white box overlay height

--- app/components/home/PopularBrands.tsx
+++ app/components/home/PopularBrands.tsx
@@ -8,7 +8,7 @@ export default function PopularBrands() {
   const bgColor = theme === 'dark' ? 'bg-t_black' : 'bg-t_white'
 
   return (
-    <section className={`${textColor} bg-t_mustard w-full max-w-full relative mt-5`}>
+    <section className={`${textColor} bg-t_mustard w-full max-w-full relative mt-7`}>
       <div className={`absolute top-0 left-0 right-0 z-10 ${bgColor} h-[6vw] sm:h-[4.5vw] lg:h-[3vw] -mt-2`}></div>
       <span className={`text-[13.8vw] lg:text-[8.5vw] lg:ml-[2rem] lg:tracking-[3rem] font-alokary block text-center`}>
         POPULAR
@@ -16,10 +16,10 @@ export default function PopularBrands() {
 
       <PopularBrandsImages />
 
-      <span className="text-[15.5vw] lg:text-[8.5vw] lg:ml-[4rem] lg:tracking-[5rem] font-alokary block text-center">
+      <span className="text-[15.5vw] lg:text-[8.5vw] lg:ml-[4rem] lg:tracking-[5rem] font-alokary block text-center -mt-1">
         BRANDS
       </span>
-      <div className={`absolute bottom-0 left-0 right-0 z-10 ${bgColor} h-[4vw] sm:h-[3.2vw] lg:h-[1.7vw] -mb-2`}></div>
+      <div className={`absolute bottom-0 left-0 right-0 z-10 ${bgColor} h-[4.2vw] sm:h-[3vw] lg:h-[1.7vw] -mb-2`}></div>
     </section>
   )
 }
