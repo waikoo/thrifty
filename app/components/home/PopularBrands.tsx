67b25b27333f71f1b0701c251230a2c2fc421934
waikoo <beroer@protonmail.com>
2024-04-22 20:35:31 +0300

    Style: adjust height between popular text and images

--- app/components/home/PopularBrands.tsx
+++ app/components/home/PopularBrands.tsx
@@ -10,7 +10,7 @@ export default function PopularBrands() {
   return (
     <section className={`${textColor} bg-t_mustard w-full max-w-full relative mt-7`}>
       <div className={`absolute top-0 left-0 right-0 z-10 ${bgColor} h-[6vw] sm:h-[4.5vw] lg:h-[3vw] -mt-2`}></div>
-      <span className={`text-[13.8vw] lg:text-[8.5vw] lg:ml-[2rem] lg:tracking-[3rem] font-alokary block text-center`}>
+      <span className={`text-[13.8vw] lg:text-[8.5vw] lg:ml-[2rem] lg:tracking-[3rem] font-alokary block text-center mb-3`}>
         POPULAR
       </span>
 
