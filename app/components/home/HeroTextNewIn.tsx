6cb365acd7dd41b3558679e3a4c6f0cbbff96fb1
waikoo <beroer@protonmail.com>
2024-04-22 20:28:55 +0300

    Style: adjust position

--- app/components/home/HeroTextNewIn.tsx
+++ app/components/home/HeroTextNewIn.tsx
@@ -1,7 +1,7 @@
 export default function HeroTextNewIn() {
 
   return (
-    <p className="left-0 text-center font-alokary right-0 absolute bottom-[8rem] text-[2.5rem] md:text-[90px] xl:text-[5.5rem] xl:bottom-[4.5rem] font-bold leading-none text-t_purple text-shadow shadow-black xl:left-[50rem] tracking-widest">
+    <p className="left-0 text-center font-alokary right-0 absolute bottom-[12.5rem] text-[2.5rem] md:text-[90px] xl:text-[5.5rem] xl:bottom-[4.5rem] font-bold leading-none text-t_purple text-shadow shadow-black xl:left-[50rem] tracking-widest">
       <span className="block -mb-2 md:mb-7">RADI<span className="sm:block sm:mt-3 md:mt-7 xl:mt-8">ANT</span></span>
       <span className="block mt-6 md:mb-7 xl:mt-10">REVI<span className="sm:block sm:mt-3 md:mt-7 xl:mt-10">VAL</span></span>
     </p>
