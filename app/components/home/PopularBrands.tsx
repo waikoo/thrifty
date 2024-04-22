2c5fe2ce5c75664d55392079a90bab39173b94d3
waikoo <beroer@protonmail.com>
2024-04-22 14:56:47 +0300

    Style: PopularBrands text blends into background on all viewports

--- app/components/home/PopularBrands.tsx
+++ app/components/home/PopularBrands.tsx
@@ -8,18 +8,18 @@ export default function PopularBrands() {
   const bgColor = theme === 'dark' ? 'bg-t_black' : 'bg-t_white'
 
   return (
-    <section className={`${textColor} bg-t_mustard w-full max-w-full relative`}>
-      <div className={`absolute top-0 left-0 right-0 z-10 ${bgColor} h-10 -mt-2`}></div>
-      <span className={`text-[13.8vw] font-alokary block text-center`}>
+    <section className={`${textColor} bg-t_mustard w-full max-w-full relative mt-5`}>
+      <div className={`absolute top-0 left-0 right-0 z-10 ${bgColor} h-[6vw] sm:h-[4.5vw] lg:h-[3vw] -mt-2`}></div>
+      <span className={`text-[13.8vw] lg:text-[8.5vw] lg:ml-[2rem] lg:tracking-[3rem] font-alokary block text-center`}>
         POPULAR
       </span>
 
       <PopularBrandsImages />
 
-      <span className="text-[15.5vw] font-alokary block text-center">
+      <span className="text-[15.5vw] lg:text-[8.5vw] lg:ml-[4rem] lg:tracking-[5rem] font-alokary block text-center">
         BRANDS
       </span>
-      <div className={`absolute bottom-0 left-0 right-0 z-10 ${bgColor} h-6 -mb-2`}></div>
+      <div className={`absolute bottom-0 left-0 right-0 z-10 ${bgColor} h-[4vw] sm:h-[3.2vw] lg:h-[1.7vw] -mb-2`}></div>
     </section>
   )
 }
