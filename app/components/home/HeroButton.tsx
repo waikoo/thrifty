041c0ca3202c9573ab4bd1f86a5502f8165b9cc8
waikoo <beroer@protonmail.com>
2024-04-22 20:27:32 +0300

    Style: modify button size & font size & position

--- app/components/home/HeroButton.tsx
+++ app/components/home/HeroButton.tsx
@@ -16,7 +16,7 @@ export default function HeroButton({ gender, lang }: HeroButtonProps) {
   return (
     <Link href={`/${lang}/${gender}/products/?gender=${gender}&shop-by=${param}&sort-by=newfirst&page=1`}>
       <button
-        className={`absolute left-0 right-0 bottom-10 bg-t_black text-t_white py-[0.5rem] w-[138px] mx-auto rounded-full font-medium text-[0.8125rem] tracking-widest ${albert.className}`}
+        className={`absolute left-0 right-0 bottom-[5rem] bg-t_black text-t_white py-[0.5rem] md:py-[0.8rem] md:px-[1.5rem] whitespace-nowrap w-[138px] md:w-[180px] mx-auto rounded-full font-medium text-[0.8125rem] md:text-[1.125rem] tracking-widest ${albert.className}`}
       >
         SHOP NOW
       </button>
