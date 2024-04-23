a0e7e53064d8790c1b6b4d10fe9079b3e84973b2
waikoo <beroer@protonmail.com>
2024-04-23 18:16:39 +0300

    Style: fix positioning on mobile

--- app/components/home/HeroButton.tsx
+++ app/components/home/HeroButton.tsx
@@ -16,7 +16,7 @@ export default function HeroButton({ gender, lang }: HeroButtonProps) {
   return (
     <Link href={`/${lang}/${gender}/products/?gender=${gender}&shop-by=${param}&sort-by=newfirst&page=1`}>
       <button
-        className={`absolute left-0 right-0 bottom-[5rem] bg-t_black text-t_white py-[0.5rem] md:py-[0.8rem] md:px-[1.5rem] whitespace-nowrap w-[138px] md:w-[180px] mx-auto rounded-full font-medium text-[0.8125rem] md:text-[1.125rem] xl:text-[1rem] tracking-widest ${albert.className}`}
+        className={`absolute left-0 right-0 bottom-[2.5rem] md:bottom-[5rem] bg-t_black text-t_white py-[0.5rem] md:py-[0.8rem] md:px-[1.5rem] whitespace-nowrap w-[138px] md:w-[180px] mx-auto rounded-full font-medium text-[0.8125rem] md:text-[1.125rem] xl:text-[1rem] tracking-widest ${albert.className}`}
       >
         SHOP NOW
       </button>
