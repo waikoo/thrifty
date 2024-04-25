55e7abb03990dac42b02c63d4d1e801aa66fb847
waikoo <beroer@protonmail.com>
2024-04-25 14:17:14 +0300

    Refactor: use map over array of component data instead of individually calling component instances

--- app/components/data/home.ts
+++ app/components/data/home.ts
@@ -1,3 +1,13 @@
+import AdidasSVG from '@/app/components/home/icons/AdidasSVG'
+import CalvinKleinSVG from '@/app/components/home/icons/CalvinKleinSVG'
+import VansSVG from '@/app/components/home/icons/VansSVG'
+import NikeSVG from '@/app/components/home/icons/NikeSVG'
+import MangoSVG from '@/app/components/home/icons/MangoSVG'
+import HMSVG from '@/app/components/home/icons/HMSVG'
+import ZaraSVG from '@/app/components/home/icons/ZaraSVG'
+import LacosteSVG from '@/app/components/home/icons/LacosteSVG'
+import ConverseSVG from '@/app/components/home/icons/ConverseSVG'
+
 export const heroAlt = {
   new_in: {
     men: "a short haired man in a jacket looking away from the camera towards a forest",
@@ -10,3 +20,53 @@ export const heroAlt = {
     kids: "a short sleeved white t-shirt with black stripes hanging from a hanger",
   }
 }
+
+export const popularBrands = [{
+  brand: 'adidas',
+  SVG: AdidasSVG,
+  alt: 'a woman wearing a green striped adidas jacket',
+  className: 'row-start-2 col-start-2 col-end-3',
+},
+{
+  brand: 'calvin+klein',
+  SVG: CalvinKleinSVG,
+  alt: 'calvin klein jeans from the back',
+  className: 'row-start-2 col-start-3 col-end-4',
+}, {
+  brand: 'vans',
+  SVG: VansSVG,
+  alt: 'a woman wearing a black and white vans jacket',
+  className: 'row-start-2 col-start-4 col-end-5',
+},
+{
+  brand: 'nike',
+  SVG: NikeSVG,
+  alt: 'a white Nike shoe hanging from its shoelaces reflected in blue tiles',
+  className: 'row-start-3 col-start-2 col-end-3 xl:col-start-5 xl:col-end-6 xl:row-start-2',
+},
+{
+  brand: 'mango',
+  SVG: MangoSVG,
+  alt: 'a man wearing a green long sleeve shirt and a woman in an orange top in a rocky backdrop',
+  className: 'row-start-3 col-start-3 col-end-4 xl:row-start-2 xl:col-start-6 xl:col-end-7'
+}, {
+  brand: 'h&m',
+  SVG: HMSVG,
+  alt: 'a woman facing away but turning back her head toward the camera in a khaki trench coat',
+  className: 'row-start-3 col-start-4 col-end-5 xl:row-start-3 xl:col-start-2 xl:col-end-3'
+}, {
+  brand: 'zara',
+  SVG: ZaraSVG,
+  alt: 'a pair of black boots on a white background',
+  className: 'row-start-4 col-start-2 col-end-3 xl:row-start-3 xl:col-start-3 xl:col-end-4'
+}, {
+  brand: 'lacoste',
+  SVG: LacosteSVG,
+  alt: 'a man and a woman looking at the camera in baby blue lacoste jacket',
+  className: 'row-start-4 col-start-3 col-end-4 xl:row-start-3 xl:col-start-4 xl:col-end-5'
+}, {
+  brand: 'converse',
+  SVG: ConverseSVG,
+  alt: 'two pairs of feet in the classic low-top black and white converse shoes',
+  className: 'relative row-start-4 col-start-4 col-end-5 xl:row-start-3 xl:col-start-5 xl:col-end-6'
+}]
--- app/components/home/PopularBrandsImages.tsx
+++ app/components/home/PopularBrandsImages.tsx
@@ -1,16 +1,6 @@
 "use client"
-/* eslint-disable @next/next/no-img-element */
-import AdidasSVG from '@/app/components/home/icons/AdidasSVG'
-import CalvinKleinSVG from '@/app/components/home/icons/CalvinKleinSVG'
-import VansSVG from '@/app/components/home/icons/VansSVG'
-import NikeSVG from '@/app/components/home/icons/NikeSVG'
-import MangoSVG from '@/app/components/home/icons/MangoSVG'
-import HMSVG from '@/app/components/home/icons/HMSVG'
-import ZaraSVG from '@/app/components/home/icons/ZaraSVG'
-import LacosteSVG from '@/app/components/home/icons/LacosteSVG'
-import ConverseSVG from '@/app/components/home/icons/ConverseSVG'
 import GapSVG from '@/app/components/home/icons/GapSVG'
-
+import { popularBrands } from '@/app/components/data/home'
 import { useThemeStore } from '@/state/themeState'
 import { viewport } from '@/app/components/data/universalStyles'
 import useViewport from '@/app/components/hooks/useViewport'
@@ -30,94 +20,18 @@ export default function PopularBrandsImages({ lang, gender }: PopularBrandsImage
 
   return (
     <div className={`grid ${grid} justify-items-center *:rounded-[2.5rem] ${gap} md:text-[0.3125rem]`}>
-
-      <PopularBrandItem
-        brand="adidas"
-        lang={lang}
-        gender={gender}
-        theme={theme}
-        className="row-start-2 col-start-2 col-end-3"
-        alt="a woman wearing a green striped adidas jacket"
-        SVG={AdidasSVG}
-      />
-
-      <PopularBrandItem
-        brand="calvin+klein"
-        lang={lang}
-        gender={gender}
-        theme={theme}
-        className="row-start-2 col-start-3 col-end-4"
-        alt="calvin klein jeans from the back"
-        SVG={CalvinKleinSVG}
-      />
-
-      <PopularBrandItem
-        brand="vans"
-        lang={lang}
-        gender={gender}
-        theme={theme}
-        className="row-start-2 col-start-4 col-end-5"
-        alt="two black vans shoes on two pairs of feet"
-        SVG={VansSVG}
-      />
-
-      <PopularBrandItem
-        brand="nike"
-        lang={lang}
-        gender={gender}
-        theme={theme}
-        className="row-start-3 col-start-2 col-end-3 xl:col-start-5 xl:col-end-6 xl:row-start-2"
-        alt="a white Nike shoe hanging from its shoelaces reflected in blue tiles"
-        SVG={NikeSVG}
-      />
-
-      <PopularBrandItem
-        brand="mango"
-        lang={lang}
-        gender={gender}
-        theme={theme}
-        className="row-start-3 col-start-3 col-end-4 xl:row-start-2 xl:col-start-6 xl:col-end-7"
-        alt="a man wearing a green long sleeve shirt and a woman in an orange top in a rocky backdrop"
-        SVG={MangoSVG}
-      />
-      <PopularBrandItem
-        brand="h&m"
-        lang={lang}
-        gender={gender}
-        theme={theme}
-        className="row-start-3 col-start-4 col-end-5 xl:row-start-3 xl:col-start-2 xl:col-end-3"
-        alt="a woman facing away but turning back her head toward the camera in a khaki trench coat"
-        SVG={HMSVG}
-      />
-
-      <PopularBrandItem
-        brand="zara"
-        lang={lang}
-        gender={gender}
-        theme={theme}
-        className="row-start-4 col-start-2 col-end-3 xl:row-start-3 xl:col-start-3 xl:col-end-4"
-        alt="a pair of black boots on a white background"
-        SVG={ZaraSVG}
-      />
-
-      <PopularBrandItem
-        brand="lacoste"
-        lang={lang}
-        gender={gender}
-        theme={theme}
-        className="row-start-4 col-start-3 col-end-4 xl:row-start-3 xl:col-start-4 xl:col-end-5"
-        alt="a man and a woman looking at the camera in baby blue lacoste jackets"
-        SVG={LacosteSVG}
-      />
-      <PopularBrandItem
-        brand="converse"
-        lang={lang}
-        gender={gender}
-        theme={theme}
-        className="relative row-start-4 col-start-4 col-end-5 xl:row-start-3 xl:col-start-5 xl:col-end-6"
-        alt="two pairs of feet in the classic low-top black and white converse shoes"
-        SVG={ConverseSVG}
-      />
+      {popularBrands.map((brand) => (
+        <PopularBrandItem
+          key={brand.brand}
+          brand={brand.brand}
+          lang={lang}
+          gender={gender}
+          theme={theme}
+          className={brand.className}
+          alt={brand.alt}
+          SVG={brand.SVG}
+        />
+      ))}
 
       {viewportWidth >= viewport.xl &&
         <PopularBrandItem
