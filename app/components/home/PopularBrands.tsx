551c1226de437c7ab79d0dd0a2f98ad9d2a1772b
waikoo <beroer@protonmail.com>
2024-04-25 14:49:14 +0300

    Refactor: make PopularBrandsImages a server component

--- app/components/home/PopularBrands.tsx
+++ app/components/home/PopularBrands.tsx
@@ -2,6 +2,7 @@
 import { useThemeStore } from '@/state/themeState'
 import PopularBrandsImages from '@/app/components/home/PopularBrandsImages'
 import { Gender, Locales } from '@/types/link'
+import PopularBrandsText from '@/app/components/home/PopularBrandsText'
 
 type PopularBrandsProps = {
   lang: Locales
@@ -10,23 +11,11 @@ type PopularBrandsProps = {
 
 export default function PopularBrands({ lang, gender }: PopularBrandsProps) {
   const { theme } = useThemeStore()
-  const textColor = theme === 'dark' ? 'text-t_black' : 'text-t_white'
-  const bgColor = theme === 'dark' ? 'bg-t_black' : 'bg-t_white'
 
   return (
-    <section className={`${textColor} bg-t_mustard w-full max-w-full relative mt-7`}>
-      <div className={`absolute top-0 left-0 right-0 z-10 ${bgColor} h-[6vw] sm:h-[4.5vw] lg:h-[3vw] -mt-2`}></div>
-      <span className={`text-[13.8vw] lg:text-[8.5vw] lg:ml-[2rem] lg:tracking-[3rem] font-alokary block text-center mb-3`}>
-        POPULAR
-      </span>
-
-      <PopularBrandsImages lang={lang} gender={gender} />
-
-      <span className="text-[15.5vw] lg:text-[8.5vw] lg:ml-[4rem] lg:tracking-[5rem] font-alokary block text-center -mt-1">
-        BRANDS
-      </span>
-      <div className={`absolute bottom-0 left-0 right-0 z-10 ${bgColor} h-[4.2vw] sm:h-[3vw] lg:h-[1.7vw] -mb-2`}></div>
-    </section>
+    <PopularBrandsText theme={theme}>
+      <PopularBrandsImages lang={lang} gender={gender} theme={theme} />
+    </PopularBrandsText>
   )
 }
 
--- app/components/home/PopularBrandsImages.tsx
+++ app/components/home/PopularBrandsImages.tsx
@@ -1,25 +1,18 @@
-"use client"
 import GapSVG from '@/app/components/home/icons/GapSVG'
 import { popularBrands } from '@/app/components/data/home'
-import { useThemeStore } from '@/state/themeState'
-import { viewport } from '@/app/components/data/universalStyles'
-import useViewport from '@/app/components/hooks/useViewport'
 import { Gender, Locales } from '@/types/link'
 import PopularBrandItem from './PopularBrandItem'
 
 type PopularBrandsImagesProps = {
   lang: Locales
   gender: Gender
+  theme: 'light' | 'dark'
 }
 
-export default function PopularBrandsImages({ lang, gender }: PopularBrandsImagesProps) {
-  const viewportWidth = useViewport()
-  const { theme } = useThemeStore()
-  const grid = viewportWidth < viewport.xl ? 'grid-cols-[5vw_auto_auto_auto_5vw] grid-rows-[auto_1fr_1fr_1fr_auto]' : 'grid-cols-[5vw_auto_auto_auto_auto_auto_5vw] grid-rows-[auto_1fr_1fr_auto]'
-  const gap = viewportWidth > viewport.md ? 'gap-[0.3125rem]' : 'gap-[0.1875rem]'
+export default function PopularBrandsImages({ lang, gender, theme }: PopularBrandsImagesProps) {
 
   return (
-    <div className={`grid ${grid} justify-items-center *:rounded-[2.5rem] ${gap} md:text-[0.3125rem]`}>
+    <div className={`grid grid-cols-[5vw_auto_auto_auto_5vw] grid-rows-[auto_1fr_1fr_1fr_auto] xl:grid-cols-[5vw_auto_auto_auto_auto_auto_5vw] xl:grid-rows-[auto_1fr_1fr_auto] gap-[0.1875rem] md:gap-[0.3125rem] justify-items-center *:rounded-[2.5rem] md:text-[0.3125rem]`}>
       {popularBrands.map((brand) => (
         <PopularBrandItem
           key={brand.brand}
@@ -33,17 +26,15 @@ export default function PopularBrandsImages({ lang, gender }: PopularBrandsImage
         />
       ))}
 
-      {viewportWidth >= viewport.xl &&
-        <PopularBrandItem
-          brand="gap"
-          lang={lang}
-          gender={gender}
-          theme={theme}
-          className="xl:row-start-3 xl:col-start-6 xl:col-end-7"
-          alt="a woman and a man standing back to back in white t-shirts"
-          SVG={GapSVG}
-        />
-      }
+      <PopularBrandItem
+        brand="gap"
+        lang={lang}
+        gender={gender}
+        theme={theme}
+        className="hidden xl:block xl:row-start-3 xl:col-start-6 xl:col-end-7"
+        alt="a woman and a man standing back to back in white t-shirts"
+        SVG={GapSVG}
+      />
     </div>
   )
 }
