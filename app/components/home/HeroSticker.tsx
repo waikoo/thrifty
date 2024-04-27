519fdb7fd14dac58ef92ee6e0d44fa7687de33ea
waikoo <beroer@protonmail.com>
2024-04-27 16:06:28 +0300

    Style: fix positioning on 2xl and 3xl viewports

--- app/components/home/HeroSticker.tsx
+++ app/components/home/HeroSticker.tsx
@@ -11,8 +11,8 @@ export default function HeroSticker() {
   }
 
   return (
-    <div className={`absolute top-[4.6rem] left-6 ${newInOrSale.bg} ${newInOrSale.text} w-[86px] h-[86px] grid place-items-center rounded-full md:left-[3rem] sm:w-[200px] sm:h-[200px] sm:top-[3.5rem] xl:w-[180px] xl:h-[180px] xl:top-10 xl:left-10`}>
-      <span className="text-[1.3rem] sm:text-[3.9rem] font-bold font-futura tracking-tight lg:text-[3.4375rem]">{getText(heroState)}</span>
+    <div className={`absolute top-[4.6rem] left-6 ${newInOrSale.bg} ${newInOrSale.text} w-[86px] h-[86px] grid place-items-center rounded-full md:left-[3rem] sm:w-[200px] sm:h-[200px] sm:top-[3.5rem] xl:w-[180px] xl:h-[180px] xl:top-10 xl:left-10 2xl:w-[250px] 2xl:h-[250px]`}>
+      <span className="text-[1.3rem] sm:text-[3.9rem] font-bold font-futura tracking-tight lg:text-[3.4375rem] 2xl:text-[4rem]">{getText(heroState)}</span>
     </div>
   )
 }
