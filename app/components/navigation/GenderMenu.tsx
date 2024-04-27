7cf3ccab80babcefeeca0498864ea580ef092507
waikoo <beroer@protonmail.com>
2024-04-27 14:15:48 +0300

    Style: when hovering over kids, the grid is of 5 columns long, images always in last column

--- app/components/navigation/GenderMenu.tsx
+++ app/components/navigation/GenderMenu.tsx
@@ -10,7 +10,7 @@ import { useGenderStore } from "@/state/client/genderState"
 export default function GenderMenu() {
   const { gender: hoveredGender, setShowGenderMenu } = useGenderStore()
   const divRef = useRef<HTMLDivElement | null>(null)
-  const grid = hoveredGender === 'kids' ? 'grid-cols-4' : 'grid-cols-5'
+
   const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (e.target === divRef.current) { // makes popup disappear on the bottom
       setShowGenderMenu(false)
@@ -21,17 +21,19 @@ export default function GenderMenu() {
     <section className="absolute inset-0 top-[7.9rem] h-screen bg-black bg-opacity-50 backdrop-blur-sm z-50 mx-auto">
       <div className="border-darkgrey absolute inset-x-0 left-[-15rem] z-50 w-screen border-t-[0.05rem]"></div>
       <div
-        className={`bg-t_white dark:bg-t_black min-w-screen inset-x-0 z-40 mx-auto grid justify-content-center ${grid} overflow-y-hidden px-12 py-12 w-full`}
+        className={`bg-t_white dark:bg-t_black max-w-screen inset-x-0 z-40 mx-auto overflow-y-hidden px-12 py-12 w-full`}
         onMouseLeave={(e) => handleMouseLeave(e as React.MouseEvent<HTMLDivElement, MouseEvent>)}
         onMouseEnter={() => setShowGenderMenu(true)}
         ref={divRef}
       >
 
-        {hoveredGender && <GenderMember hoveredGender={hoveredGender} />}
+        <div className="grid justify-content-center grid-cols-5 max-w-[1600px] mx-auto">
+          {hoveredGender && <GenderMember hoveredGender={hoveredGender} />}
 
-        <div className="my-auto flex flex-col gap-10">
-          <GenderImage img={women_01} className="col-start-5 col-end-6 row-start-1 w-full">NEW IN</GenderImage>
-          <GenderImage img={women_02} className="col-start-5 col-end-6 row-start-2 w-full">PROMOS</GenderImage>
+          <div className="my-auto flex flex-col gap-10 col-start-5 col-end-6">
+            <GenderImage img={women_01} className="col-start-5 col-end-6 row-start-1 w-full">NEW IN</GenderImage>
+            <GenderImage img={women_02} className="col-start-5 col-end-6 row-start-2 w-full">PROMOS</GenderImage>
+          </div>
         </div>
       </div >
     </section>
