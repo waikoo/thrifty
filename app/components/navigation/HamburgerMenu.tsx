9c6438a26b77547111ffdb59a2e6892f36e791f0
waikoo <beroer@protonmail.com>
2024-04-29 18:41:12 +0300

    Add: 80% HamburgerMenu width, when click on 20%, menu closes

--- app/components/navigation/HamburgerMenu.tsx
+++ app/components/navigation/HamburgerMenu.tsx
@@ -1,14 +1,25 @@
+"use client"
 import { albert_600 } from "@/utils/fonts";
 import IconAccount from "./icons/IconAccount";
 import { RxCross2 } from "react-icons/rx";
 import { useNavigationStore } from "@/state/client/navigationState";
+import { useRef } from "react";
 
 export default function HamburgerMenu() {
   const { setShowHamburgerMenu } = useNavigationStore()
+  const bgRef = useRef(null)
 
+  const handleCloseHamburgerMenu = (e: React.MouseEvent) => {
+    if (e.target === bgRef.current) {
+      setShowHamburgerMenu(false)
+    }
+  }
   return (
-    <section className="absolute inset-0 z-[60] bg-t_white dark:bg-t_black">
-      <div className="p-4">
+    <section className="absolute inset-0 z-[60] h-screen w-screen bg-[rgba(0,0,0,0.5)]"
+      ref={bgRef}
+      onClick={handleCloseHamburgerMenu}
+    >
+      <div className="p-4 w-[80%] bg-t_white dark:bg-t_black">
 
         <div className="grid grid-cols-2 w-full">
           <div className="flex gap-2 items-center">
