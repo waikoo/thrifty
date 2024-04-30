a25fb41e04f4fc4d3a1f93afa4417b76f28cf437
waikoo <beroer@protonmail.com>
2024-04-30 18:18:04 +0300

    Change: category to gender, use utils getLangAndGender() fn

--- app/components/navigation/LanguageElement.tsx
+++ app/components/navigation/LanguageElement.tsx
@@ -1,19 +1,21 @@
 "use client"
-import { usePathname } from "next/dist/client/components/navigation";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import getLangAndGender from "@/utils/getLangAndGender";
 
 type LanguageElementProps = {
   locale: string,
 }
 
 export default function LanguageElement({ locale }: LanguageElementProps) {
-  const [, lang, category] = usePathname().split('/')
+  const { lang, gender } = getLangAndGender(usePathname())
   const selectedLocale = lang !== locale ? "text-t_grey font-medium" : "font-extrabold"
   const increaseSpace = lang === 'de' ? 'pl-1/2' : ''
   const uppercaseLocale = locale.toUpperCase()
 
   return (
-    <Link href={`/${locale}/${category}`}
+    <Link href={`/${locale}/${gender}`}
       className={`items-center relative ${selectedLocale} ${increaseSpace} `}>
       {uppercaseLocale}
     </Link>
