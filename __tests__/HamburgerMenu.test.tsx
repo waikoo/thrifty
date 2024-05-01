63b5e2e5b6e72fb31345ba9c5d396e98c88ade17
waikoo <beroer@protonmail.com>
2024-05-01 18:52:10 +0300

    Change: clean up test

--- __tests__/HamburgerMenu.test.tsx
+++ __tests__/HamburgerMenu.test.tsx
@@ -1,18 +1,17 @@
-import Page from "@/app/[lang]/(client)/[gender]/page"
 import NavBarMobile from "@/app/components/navigation/NavBarMobile"
 import { render, screen } from "@testing-library/react"
 import userEvent from '@testing-library/user-event'
 
 describe('Client: HamburgerMenu', () => {
 
-  test('in NavBarMobile the hamburgers parentelement renders', () => {
+  test("in NavBarMobile the hamburger icon renders", () => {
     render(<NavBarMobile />)
     const hamburgerIcon = screen.getByTitle(/hamburger menu/i)
 
     expect(hamburgerIcon).toBeInTheDocument()
   })
 
-  it('should render on click from NavBarMobile', async () => {
+  test('HamburgerMenu should render on hamburger icon click from NavBarMobile', async () => {
     const user = userEvent.setup()
 
     render(<NavBarMobile />)
