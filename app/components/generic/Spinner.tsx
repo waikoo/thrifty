4c77eebbf9002fa44840804d1fafe5c9cebabcc0
waikoo <beroer@protonmail.com>
2024-05-03 23:57:27 +0300

    Change: Spinner color to mustard

--- app/components/generic/Spinner.tsx
+++ app/components/generic/Spinner.tsx
@@ -18,7 +18,7 @@ function Spinner() {
       <g transform="translate(80 50)">
         <circle
           r="6"
-          fill="#2eaa4f"
+          fill="#d3d62e"
         // style={{ DarkreaderInlineFill: "#99ffc3" }}
         >
           <animateTransform
@@ -43,7 +43,7 @@ function Spinner() {
       <g transform="translate(71.213 71.213) rotate(45)">
         <circle
           r="6"
-          fill="#2eaa4f"
+          fill="#d3d62e"
           fillOpacity="0.875"
         // style={{ DarkreaderInlineFill: "#31b754" }}
         >
@@ -69,7 +69,7 @@ function Spinner() {
       <g transform="translate(50 80) rotate(90)">
         <circle
           r="6"
-          fill="#2eaa4f"
+          fill="#d3d62e"
           fillOpacity="0.75"
         // style={{ DarkreaderInlineFill: "#99ffc3" }}
         >
@@ -95,7 +95,7 @@ function Spinner() {
       <g transform="translate(28.787 71.213) rotate(135)">
         <circle
           r="6"
-          fill="#2eaa4f"
+          fill="#d3d62e"
           fillOpacity="0.625"
         // style={{ DarkreaderInlineFill: "#31b754" }}
         >
@@ -121,7 +121,7 @@ function Spinner() {
       <g transform="translate(20 50) rotate(180)">
         <circle
           r="6"
-          fill="#2eaa4f"
+          fill="#d3d62e"
           fillOpacity="0.5"
         // style={{ DarkreaderInlineFill: "#99ffc3" }}
         >
@@ -147,7 +147,7 @@ function Spinner() {
       <g transform="translate(28.787 28.787) rotate(225)">
         <circle
           r="6"
-          fill="#2eaa4f"
+          fill="#d3d62e"
           fillOpacity="0.375"
         // style={{ DarkreaderInlineFill: "#31b754" }}
         >
@@ -173,7 +173,7 @@ function Spinner() {
       <g transform="translate(50 20) rotate(270)">
         <circle
           r="6"
-          fill="#2eaa4f"
+          fill="#d3d62e"
           fillOpacity="0.25"
         // style={{ DarkreaderInlineFill: "#99ffc3" }}
         >
@@ -199,7 +199,7 @@ function Spinner() {
       <g transform="translate(71.213 28.787) rotate(315)">
         <circle
           r="6"
-          fill="#2eaa4f"
+          fill="#d3d62e"
           fillOpacity="0.125"
         // style={{ DarkreaderInlineFill: "#31b754" }}
         >
