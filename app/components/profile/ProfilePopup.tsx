0d56df1269951d7b4fc5d514d72ba9a52306b35b
waikoo <beroer@protonmail.com>
2024-05-10 22:04:09 +0300

    Style: /profile email & password change to mustard theme

--- app/components/profile/ProfilePopup.tsx
+++ app/components/profile/ProfilePopup.tsx
@@ -31,14 +31,15 @@ export default function ProfilePopup({ children, setShowPopup, inputSettings, up
   }
 
   return (
-    <div className="text-bkg absolute inset-0 z-[60] grid h-screen w-screen place-items-center bg-[rgba(0,0,0,0.5)]" onClick={closePopup} ref={outerRef}>
-      <div className=" bg-content relative flex w-[300px] flex-col gap-6 p-7 opacity-100">
-        <h2 className="text-center text-[1rem] font-extrabold">CHANGE {children}</h2>
-        <IoCloseOutline color='black' size={25} className="absolute right-1 top-1 cursor-pointer" onClick={() => setShowPopup(false)} />
+    <div className="absolute inset-0 z-[60] grid h-screen w-screen place-items-center bg-[rgba(0,0,0,0.5)]" onClick={closePopup} ref={outerRef}>
+      <div className="text-t_black bg-t_white relative flex w-[350px] flex-col gap-6 p-8 py-12 opacity-100 border-[0.5rem] border-t_mustard rounded-[2.1875rem]">
+        <IoCloseOutline color='black' size={25} className="absolute right-3 top-3 cursor-pointer" onClick={() => setShowPopup(false)} />
 
-        {inputSettings.map((setting) => <AnimatedInput key={setting.id} {...setting} />)}
+        <h2 className="text-center text-[18px] font-extrabold">CHANGE {children}</h2>
 
-        <button className="bg-bkg text-content mt-10 rounded-full py-3 text-[0.8125rem] font-semibold" onClick={submit}>SAVE</button>
+        {inputSettings.map((setting) => <AnimatedInput key={setting.id} font="text-[#9d9d9d]" {...setting} />)}
+
+        <button className="bg-t_black text-t_white mt-5 rounded-full py-3 text-[0.8125rem] font-semibold" onClick={submit}>SAVE</button>
       </div>
     </div>
   )
