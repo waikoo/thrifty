8b5dc44a026bb1129ebdd99d973e69347671cc3f
waikoo <beroer@protonmail.com>
2024-05-13 21:56:31 +0300

    Fix: Hero swipe

--- app/components/hooks/useHeroSwipe.tsx
+++ app/components/hooks/useHeroSwipe.tsx
@@ -1,42 +1,39 @@
-import { useHomeStore } from "@/state/client/homeState"
-import { useState } from "react"
+import { useHomeStore } from "@/state/client/homeState";
+import { useEffect, useState } from "react";
 
 export default function useHeroSwipe() {
-  const [startX, setStartX] = useState(0)
-  const [endX, setEndX] = useState(0)
-  const { heroState, setHeroState } = useHomeStore()
+  const [startX, setStartX] = useState(0);
+  const [endX, setEndX] = useState(0);
+  const { heroState, setHeroState } = useHomeStore();
 
   function touchStartHandler(e: React.TouchEvent): void {
-    setStartX(e.touches[0].clientX)
+    setStartX(e.touches[0].clientX);
   }
 
   function touchEndHandler(e: React.TouchEvent): void {
-    setEndX(e.changedTouches[0].clientX)
-    handleSwipeDirection()
+    setEndX(e.changedTouches[0].clientX);
+    handleSwipeDirection();
   }
 
   function handleSwipeDirection() {
-    const swipeDistance = endX - startX
+    const swipeDistance = endX - startX;
 
-    if (Math.abs(swipeDistance) > 20) {
+    if (Math.abs(swipeDistance) > 15) {
       if (swipeDistance > 0) {
-        if (heroState === 'sale') {
-          return
-        } else {
-          setHeroState('sale')
-        }
+        setHeroState("new_in");
       } else {
-        if (heroState === 'new_in') {
-          return
-        } else {
-          setHeroState('new_in')
-        }
+        setHeroState("sale");
       }
     }
   }
+
+  useEffect(() => {
+    setStartX(0);
+    setEndX(0);
+  }, [heroState]);
+
   return {
     touchStartHandler,
     touchEndHandler,
-  }
+  };
 }
-
