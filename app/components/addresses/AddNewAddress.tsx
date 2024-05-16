1d8f1f4e061e4efb91bc1f155bd55e48b47b97db
waikoo <beroer@protonmail.com>
2024-05-16 21:14:34 +0300

    Style: /addresses is mustard themed

--- app/components/addresses/AddNewAddress.tsx
+++ app/components/addresses/AddNewAddress.tsx
@@ -1,14 +1,17 @@
 "use client"
 import { useAddressStore } from "@/state/client/addressState";
+import { albert_900 } from "@/utils/fonts";
 import { GoPlus } from "react-icons/go";
 
 export default function AddNewAddress() {
   const { setShowAddAddress } = useAddressStore()
 
   return (
-    <div className="border-content flex  cursor-pointer flex-col items-center gap-4 border-b-[0.1rem] py-[2rem]" onClick={() => setShowAddAddress(true)}>
-      <GoPlus size={32} />
-      <span className="text-[0.75rem] font-extrabold">ADD NEW ADDRESSES</span>
+    <div className="flex flex-col items-center gap-4 border-b-[0.1rem] border-[#c2c2c2] pb-6">
+      <div className="mt-10 w-[60px] h-[60px] bg-t_mustard flex items-center justify-center cursor-pointer border-b-[0.1rem] py-[2rem] rounded-full" onClick={() => setShowAddAddress(true)}>
+        <GoPlus size={38} />
+      </div>
+      <span className={`text-[0.8125rem] ${albert_900.className}`}>ADD NEW ADDRESS</span>
     </div>
   )
 }
--- app/components/addresses/NoSavedAddress.tsx
+++ app/components/addresses/NoSavedAddress.tsx
@@ -1,9 +1,10 @@
+import { albert } from "@/utils/fonts";
 
 export default function NoSavedAddress() {
 
   return (
     <div className="mt-20 text-center">
-      <span className="text-silver text-[0.8125rem] font-medium">You currently have no saved addresses.</span>
+      <span className={`text-silver text-[0.875rem] ${albert.className} text-[#555555]`}>You currently have no saved addresses.</span>
     </div>
   )
 }
