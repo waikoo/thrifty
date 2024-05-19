80657744947ebbbb8deae8ce176025e8ad67be0d
waikoo <beroer@protonmail.com>
2024-05-19 19:28:27 +0300

    Style: adjust font size and weight on /returns

--- app/[lang]/(client)/[gender]/returns/page.tsx
+++ app/[lang]/(client)/[gender]/returns/page.tsx
@@ -1,7 +1,9 @@
+import { albert } from "@/utils/fonts";
+
 export default function Page() {
 
   return (
-    <div className="text-content my-20 text-center">No returns were found.</div>
+    <div className={`text-content my-20 ${albert.className} text-[13px] sm:text-[17px] xl:text-[11px] text-center`}>No returns were found.</div>
   )
 }
 
