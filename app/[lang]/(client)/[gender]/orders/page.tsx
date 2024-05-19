be3d2f1d55f9b6e18df12b72dee4b593a0ddfb13
waikoo <beroer@protonmail.com>
2024-05-19 19:25:26 +0300

    Style: adjust font size and weight on /orders

--- app/[lang]/(client)/[gender]/orders/page.tsx
+++ app/[lang]/(client)/[gender]/orders/page.tsx
@@ -1,6 +1,8 @@
+import { albert } from "@/utils/fonts";
+
 export default function Page() {
 
   return (
-    <div className="text-content my-20 text-center">It looks like you haven't placed any orders yet</div>
+    <div className={`text-content my-20 text-center ${albert.className} text-[13px] sm:text-[17px] xl:text-[11px] p-12`}>It looks like you haven't placed any orders yet</div>
   )
 }
