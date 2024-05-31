7e519f4ddaf0d8e07646b1e1ba9f01eb3ec17ed9
waikoo <beroer@protonmail.com>
2024-05-31 22:57:25 +0300

    Style: add photo to Contact on /help

--- app/components/help/Contact.tsx
+++ app/components/help/Contact.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import HelpTitle from "@/app/components/help/HelpTitle"
 import OpeningHours from "@/app/components/footer/OpeningHours"
 import FooterContact from "@/app/components/footer/FooterContact"
@@ -11,16 +12,25 @@ type ContactProps = {
 export default function Contact({ children, className }: ContactProps) {
 
   return (
-    <div className={`${className} p-4`}>
-      <HelpTitle>{children}</HelpTitle>
-      <OpeningHours invert={true} />
-      <br />
+    <div className={`${className} flex`}>
+      <div className={`p-4`}>
+        <HelpTitle>{children}</HelpTitle>
+        <OpeningHours invert={true} />
+        <br />
 
-      <span className={`text-[14px] ${albert_900.className} text-t_black mb-10`}>CUSTOMER SUPPORT</span>
+        <span className={`text-[14px] ${albert_900.className} text-t_black mb-10`}>CUSTOMER SUPPORT</span>
 
-      <FooterContact hideLogo={true}
-        invertSocials={false}
-      />
+        <FooterContact hideLogo={true}
+          invertSocials={false}
+        />
+      </div>
+
+      <div className="flex-grow h-dvh">
+        <img src="/images/help/contact.jpg"
+          className="object-cover h-full"
+          alt="a lit up storefront from the outside street full with colorful clothes"
+        />
+      </div>
     </div>
   )
 }
