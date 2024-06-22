db9eba8b4de94b6b6d7a15da54a449b86a33f2b3
waikoo <beroer@protonmail.com>
2024-06-22 16:06:47 +0300

    Fix: users not signed in get a sign in popup on accessing user-specific pages (profile, addresses, orders, returns, settings) from HamburgerMenu

--- app/components/navigation/HamburgerAccountAndHelp.tsx
+++ app/components/navigation/HamburgerAccountAndHelp.tsx
@@ -3,6 +3,9 @@ import Link from 'next/link'
 import { albert_500, albert_900 } from '@/utils/fonts'
 import { Gender, Locales } from '@/types/link'
 import { useNavigationStore } from '@/state/client/navigationState'
+import { supabase } from '@/app/supabase'
+import { useRouter } from 'next/navigation'
+import { useUIStore } from '@/state/client/uiState'
 
 type HamburgerAccountAndHelpProps = {
   lang: Locales
@@ -11,8 +14,21 @@ type HamburgerAccountAndHelpProps = {
 
 export default function HamburgerAccountAndHelp({ lang, gender }: HamburgerAccountAndHelpProps) {
   const { setShowHamburgerMenu } = useNavigationStore()
+  const router = useRouter()
+  const { showSignIn, setShowSignIn } = useUIStore()
 
-  const hideHamburger = () => {
+  const hideHamburger = async (e: React.MouseEvent) => {
+    const { data: { user } } = await supabase.auth.getUser()
+    const target = e.target as HTMLAnchorElement
+    const targetText = target.textContent!.toLowerCase()
+
+    if (!user) {
+      e.preventDefault()
+      setShowSignIn(!showSignIn)
+    } else {
+      router.push(`/${lang}/${gender}/${targetText}`)
+    }
+
     setShowHamburgerMenu(false)
   }
 
@@ -21,10 +37,10 @@ export default function HamburgerAccountAndHelp({ lang, gender }: HamburgerAccou
       <div className="grid gap-3">
         <span className={`block ${albert_900.className} text-[0.75rem] sm:text-[1rem] tracking-wider`}>ACCOUNT</span>
         <div className={`*:text-[0.8125rem] sm:*:text-[17px] grid gap-3 *:${albert_500.className}`}>
-          <Link onClick={hideHamburger} href={`/${lang}/${gender}/profile`}>Profile</Link>
-          <Link onClick={hideHamburger} href={`/${lang}/${gender}/orders`}>Orders</Link>
-          <Link onClick={hideHamburger} href={`/${lang}/${gender}/returns`}>Returns</Link>
-          <Link onClick={hideHamburger} href={`/${lang}/${gender}/settings`}>Settings</Link>
+          <a onClick={(e) => hideHamburger(e)}>Profile</a>
+          <a onClick={(e) => hideHamburger(e)}>Orders</a>
+          <a onClick={(e) => hideHamburger(e)}>Returns</a>
+          <a onClick={(e) => hideHamburger(e)}>Settings</a>
         </div>
       </div>
 
@@ -33,7 +49,7 @@ export default function HamburgerAccountAndHelp({ lang, gender }: HamburgerAccou
         <div className={`*:text-[0.8125rem] sm:*:text-[1.0625rem] grid gap-3 *:${albert_500.className}`}>
           <Link onClick={hideHamburger} href={`/${lang}/${gender}/help?section=0`}>About Us</Link>
           <Link onClick={hideHamburger} href={`/${lang}/${gender}/help?section=1`}>Delivery & Returns</Link>
-          <Link onClick={hideHamburger} href={`/${lang}/${gender}/help?section=4`}>Contact</Link>
+          <Link onClick={hideHamburger} href={`/${lang}/${gender}/help?section=3`}>Contact</Link>
         </div>
       </div>
     </div>
