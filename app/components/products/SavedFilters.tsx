1d1b25a7765bc95b9da7295de2cbdeebc6a7e6c8
waikoo <beroer@protonmail.com>
2024-07-06 22:44:23 +0300

    Add: mustard theme to SavedFilters (desktop) on /products

--- app/components/products/SavedFilters.tsx
+++ app/components/products/SavedFilters.tsx
@@ -14,6 +14,8 @@ import { TSavedFilters } from "@/types/filters";
 import { useThemeStore } from "@/state/themeState";
 import useUserSession from "@/app/components/hooks/useUserSession";
 import { useFilterStore } from "@/state/client/filterState";
+import WithCloseButton from "../navigation/WithCloseButton";
+import { albert, albert_500, albert_800 } from "@/utils/fonts";
 
 export default function SavedFilters() {
   const [savedFilters, setSavedFilters] = useState<TSavedFilters[]>([] as TSavedFilters[])
@@ -76,53 +78,46 @@ export default function SavedFilters() {
   }
 
   return (
-    <section className="absolute inset-0 z-[60] grid h-screen w-screen place-items-center bg-[rgba(0,0,0,0.5)]">
-      <div className="bg-bkg flex w-[400px] flex-col gap-6 p-7 opacity-100">
-        <div onClick={() => setShowSavedFiltersPopup(false)}>
-          <IconDelete size={"16"} className="ml-auto cursor-pointer" />
-        </div>
-        <h1 className="text-content text-center text-[1rem] font-extrabold">SAVED FILTERS</h1>
+    <WithCloseButton onClose={() => setShowSavedFiltersPopup(false)} padding="p-8">
+      <h1 className={`mb-3 text-t_black text-center text-[14px] ${albert_800.className}`}>SAVED FILTERS</h1>
 
-        <div className="flex items-center gap-4 rounded-full bg-gray-300 p-5 text-black">
-          <Toggle
-            type="filters"
-            thumbColor="peer-checked:after:bg-gray-500"
-            toggleBgColor="bg-gray-300"
-            thumbBorder="peer-checked:after:border-gray-500"
-            toggleBorder="border-[0.1rem] border-gray-500"
-          />
-          <p className="w-[90%]">Get notified about any changes regarding your saved filters</p>
-        </div>
-
-        {savedFilters ? savedFilters.map((filter, index) => (
-          <div key={filter.name} onClick={(e) => handleRunFilter(e, filter.filterId)} className="cursor-pointer" title="Select filter">
-            <div className="flex justify-between">
-              <p key={index} className="py-0">{filter.name}</p>
-              <div className="flex gap-2" >
-                <div ref={optionsRef} >
-                  <BsThreeDotsVertical color={color} onClick={(e: React.MouseEvent<SVGElement>) => handleShowOptions(e)} className="cursor-pointer" />
-                </div>
-                {showOptions && (
-                  <>
-                    <div onClick={() => handleEdit(filter.filterId)} >
-                      <MdEdit color={color} className="cursor-pointer" />
-                    </div>
-                    <IoMdNotifications color={color} className="cursor-pointer" onClick={() => changeNotification(filter.filterId, client_id)} />
-                    <RxCross1 color={color} className="cursor-pointer" onClick={() => deleteFilter(filter.filterId, client_id)} />
-                  </>
-                )
-                }
-              </div>
-            </div>
-            <hr />
-          </div>
-        )) : (
-          <p className="text-content text-center text-[1rem] font-extrabold">No saved filters</p>
-        )}
-
-
+      <div className="flex items-center gap-4 rounded-full bg-[#f2f2f2] p-5 text-black">
+        <Toggle
+          type="filters"
+          thumbColor="peer-checked:after:bg-t_black after:bg-t_white"
+          toggleBgColor="bg-[#c2c2c2] peer-checked:bg-t_mustard"
+          thumbBorder="peer-checked:after:border-gray-500"
+          toggleBorder="border-[0.1rem] border-gray-500"
+        />
+        <p className={`w-[90%] text-[14px] ${albert_500.className}`}>Get notified about any changes regarding your saved filters</p>
       </div>
 
-    </section>
+      {savedFilters ? savedFilters.map((filter, index) => (
+        <div key={filter.name} onClick={(e) => handleRunFilter(e, filter.filterId)} className="cursor-pointer" title="Select filter">
+          <div className="flex justify-between">
+            <p key={index} className={`py-0 text-[14px] ${albert_500.className}`}>{filter.name}</p>
+            <div className="flex gap-2" >
+              <div ref={optionsRef} >
+                <BsThreeDotsVertical color={color} onClick={(e: React.MouseEvent<SVGElement>) => handleShowOptions(e)} className="cursor-pointer" />
+              </div>
+              {showOptions && (
+                <>
+                  <div onClick={() => handleEdit(filter.filterId)} >
+                    <MdEdit color={color} className="cursor-pointer" />
+                  </div>
+                  <IoMdNotifications color={color} className="cursor-pointer" onClick={() => changeNotification(filter.filterId, client_id)} />
+                  <RxCross1 color={color} className="cursor-pointer" onClick={() => deleteFilter(filter.filterId, client_id)} />
+                </>
+              )
+              }
+            </div>
+          </div>
+          <hr />
+        </div>
+      )) : (
+        <p className={`mt-3 text-content text-center text-[14px] ${albert.className}`}>No saved filters</p>
+      )}
+
+    </WithCloseButton>
   )
 }
