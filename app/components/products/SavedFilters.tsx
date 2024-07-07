bcfbca5a48c18eb3df3380a357ea609d99a594ac
waikoo <beroer@protonmail.com>
2024-07-07 17:33:35 +0300

    Refactor: extract SavedFiltersTitle from SavedFilters

--- app/components/products/SavedFilters.tsx
+++ app/components/products/SavedFilters.tsx
@@ -1,20 +1,19 @@
 "use client"
 import { useFilterStore } from "@/state/client/filterState";
 import WithCloseButton from "@/app/components/navigation/WithCloseButton";
-import { albert_800 } from "@/utils/fonts";
 import SavedFiltersList from "@/app/components/products/SavedFiltersList";
 import SavedFiltersNotification from "@/app/components/products/SavedFiltersNotification";
+import SavedFiltersTitle from "@/app/components/products/SavedFiltersTitle";
 
 export default function SavedFilters() {
   const { setShowSavedFiltersPopup } = useFilterStore()
 
   return (
-    <WithCloseButton onClose={() => setShowSavedFiltersPopup(false)}
+    <WithCloseButton
+      onClose={() => setShowSavedFiltersPopup(false)}
       padding="p-8"
     >
-      <h1 className={`mb-3 text-t_black text-center text-[14px] ${albert_800.className}`}>
-        SAVED FILTERS
-      </h1>
+      <SavedFiltersTitle />
 
       <SavedFiltersNotification />
 
---
+++ app/components/products/SavedFiltersTitle.tsx
@@ -0,0 +1,10 @@
+import { albert_800 } from "@/utils/fonts";
+
+export default function SavedFiltersTitle() {
+  return (
+    <h1 className={`mb-3 text-t_black xl:text-center text-[14px] ${albert_800.className}`}>
+      SAVED FILTERS
+    </h1>
+  )
+}
+
