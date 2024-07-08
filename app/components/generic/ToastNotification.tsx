d5aa7df3ffc49a74f524ee818721acbcc6ac15e2
waikoo <beroer@protonmail.com>
2024-07-08 22:25:03 +0300

    Style: add font styles for tablet & desktop views to ToastNotification

--- app/components/generic/ToastNotification.tsx
+++ app/components/generic/ToastNotification.tsx
@@ -1,3 +1,4 @@
+import { albert_500 } from "@/utils/fonts";
 import { IoMdCheckmark } from "react-icons/io";
 import { RxCross2 } from "react-icons/rx";
 
@@ -9,10 +10,10 @@ export default function ToastNotification({ children }: ToastNotificationProps) 
 
   return (
     <div className={`fixed bottom-20 left-0 right-0 `}>
-      <div className="w-[380px] flex items-center gap-3 p-3 bg-[#dafcc9] rounded-full justify-center mx-auto">
-        <IoMdCheckmark />
+      <div className={`w-[380px] sm:text-[15px] xl:text-[12px] text-[#567b07] ${albert_500.className} flex items-center gap-3 p-3 bg-[#dafcc9] rounded-full justify-center mx-auto`}>
+        <IoMdCheckmark color="#567b07" size={25} />
         {children}
-        <RxCross2 />
+        <RxCross2 color="#567b07" size={25} />
       </div>
     </div>
   )
