3dacf71b3b61f0ca376829fc61d4ba1c77a29293
waikoo <beroer@protonmail.com>
2024-07-13 22:05:43 +0300

    Add: mobile text size + extract variable

--- app/components/generic/ToastNotification.tsx
+++ app/components/generic/ToastNotification.tsx
@@ -7,13 +7,15 @@ type ToastNotificationProps = {
 }
 
 export default function ToastNotification({ children }: ToastNotificationProps) {
+  const iconColor = '#567b07'
+  const iconSize = 25
 
   return (
     <div className={`fixed bottom-20 left-0 right-0 `}>
-      <div className={`w-[380px] sm:text-[15px] xl:text-[12px] text-[#567b07] ${albert_500.className} flex items-center gap-3 p-3 bg-[#dafcc9] rounded-full justify-center mx-auto`}>
-        <IoMdCheckmark color="#567b07" size={25} />
+      <div className={`w-[380px] text-[12px] sm:text-[15px] xl:text-[12px] text-[#567b07] ${albert_500.className} flex items-center gap-3 p-3 bg-[#dafcc9] rounded-full justify-center mx-auto`}>
+        <IoMdCheckmark color={iconColor} size={iconSize} />
         {children}
-        <RxCross2 color="#567b07" size={25} />
+        <RxCross2 color={iconColor} size={iconSize} />
       </div>
     </div>
   )
