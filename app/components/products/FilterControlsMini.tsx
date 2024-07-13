bbae433b577579e375e112a30140adb369d239e4
waikoo <beroer@protonmail.com>
2024-07-13 18:41:10 +0300

    Style: FilterControlsMini doesn't let the user scroll past its bottom

--- app/components/products/FilterControlsMini.tsx
+++ app/components/products/FilterControlsMini.tsx
@@ -24,8 +24,8 @@ export default function FilterControlsMini({ lang, gender, searchParams, filtere
 
   return (
     <Portal>
-      <div className="inset-0 relative"
-      >
+      <section className="fixed left-0 right-0 z-[100] overflow-y-scroll h-[calc(100dvh)]">
+
         <div className="p-5 sm:p-10 bg-t_mustard text-t_black py-1 sm:py-3 flex justify-between items-center">
           <span className={`${albert_800.className} text-[13px] sm:text-[19px]`}>FILTERS</span>
 
@@ -38,32 +38,33 @@ export default function FilterControlsMini({ lang, gender, searchParams, filtere
           </div>
         </div>
 
-      </div>
 
-      <div className="ml-auto">
-        <FilterSideControls {...{ gender, lang }}
-          className="flex sm:hidden justify-end mr-5 mt-5"
-        />
-      </div>
-
-      <div className="p-5 sm:pt-10">
-        <FilterControls {...{ searchParams }}
-          className="flex w-full overflow-y-scroll"
-          hideSaveFilter={true}
-        />
-      </div>
-
-      <div className="bg-t_mustard p-5 px-10 absolute w-full z-[90]">
-        <div className="max-w-[800px] mx-auto flex justify-between gap-6">
-          <SaveFilterButton className="w-1/2 border-t_black border-[0.1rem]" />
-
-          <button className="bg-t_black text-t_white rounded-full w-1/2"
-            onClick={closeFilters}
-          >
-            VIEW {filteredMatchesTotal} RESULTS
-          </button>
+        <div className="ml-auto bg-t_white">
+          <FilterSideControls {...{ gender, lang }}
+            className="flex sm:hidden justify-end mr-5 pt-5"
+          />
         </div>
-      </div>
+
+        <div className="p-5 sm:pt-10 bg-t_white">
+          <FilterControls {...{ searchParams }}
+            className="flex w-full overflow-y-scroll"
+            hideSaveFilter={true}
+          />
+        </div>
+
+        <div className="bg-t_mustard p-5 px-10 absolute w-full z-[90]">
+          <div className="max-w-[800px] mx-auto flex justify-between gap-6">
+            <SaveFilterButton className="w-1/2 border-t_black border-[0.1rem]" />
+
+            <button className="bg-t_black text-t_white rounded-full w-1/2"
+              onClick={closeFilters}
+            >
+              VIEW {filteredMatchesTotal} RESULTS
+            </button>
+          </div>
+        </div>
+
+      </section>
     </Portal >
   )
 }
