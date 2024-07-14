b8087a88f06c9b52b03e1f910e24fc26a579aaaa
waikoo <beroer@protonmail.com>
2024-07-14 17:05:48 +0300

    Style: adjust FilterNotFound to be responsive
    + replace svg

--- app/components/products/FilterNotFound.tsx
+++ app/components/products/FilterNotFound.tsx
@@ -1,11 +1,17 @@
-import { FilterNotFoundImg } from ".";
+import { albert_500, albert_600 } from "@/utils/fonts";
+import { FilterNotFoundImg } from "@/app/components/products";
 
 export default function FilterNotFound() {
   return (
-    <section className="mx-auto mt-10 flex w-[320px] flex-col items-center gap-6">
-      <FilterNotFoundImg />
-      <span className="text-content text-[1rem]">No Results Found</span>
-      <p className="text-center text-[0.813rem]">Hmm, we can't seem to find anything that matches your search.</p>
+    <section className="mx-auto my-10 flex flex-col items-center gap-4 xl:gap-6">
+      <div className="max-w-[75%] sm:max-w-[50%] xl:max-w-[25%] xl:w-auto">
+        <FilterNotFoundImg />
+      </div>
+
+      <div className="w-[80%] mx-auto">
+        <p className={`text-center text-t_black text-[1rem] sm:text-[21px] xl:text-[17px] ${albert_600.className}`}>No Results Found</p>
+        <p className={`mt-4 text-center text-[13px] sm:text-[17px] xl:text-[14px] text-wrap max-w-[60%] xl:max-w-[320px] mx-auto ${albert_500.className}`}>Hmm, we can't seem to find anything that matches your search.</p>
+      </div>
     </section>
   )
 }
