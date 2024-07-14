d2b69dcdd265cf8cdc5bec89722ed220e93b6f19
waikoo <beroer@protonmail.com>
2024-07-14 20:59:22 +0300

    Style: fix page showing through FilterControlsMini
    + increase height of buttons on tablet view

--- app/components/products/FilterControlsMini.tsx
+++ app/components/products/FilterControlsMini.tsx
@@ -5,8 +5,8 @@ import { Gender, Locales } from "@/types/link";
 import { useFilterStore } from "@/state/client/filterState";
 import FilterControls from "@/app/components/products/FilterControls";
 import { albert_500, albert_800 } from "@/utils/fonts";
-import SaveFilterButton from "./SaveFilterButton";
-import MiniCross from "./MiniCross";
+import SaveFilterButton from "@/app/components/products/SaveFilterButton";
+import MiniCross from "@/app/components/products/MiniCross";
 
 type FilterControlsMiniProps = {
   lang: Locales
@@ -24,7 +24,7 @@ export default function FilterControlsMini({ lang, gender, searchParams, filtere
 
   return (
     <Portal>
-      <section className="fixed left-0 right-0 z-[100] overflow-y-scroll h-[calc(100dvh)]">
+      <section className="fixed left-0 right-0 z-[100] overflow-y-scroll h-[calc(100dvh)] bg-t_white ">
 
         <div className="fixed top-0 left-0 right-0 p-5 sm:p-10 bg-t_mustard text-t_black py-1 sm:py-3 flex justify-between items-center">
           <span className={`${albert_800.className} text-[13px] sm:text-[19px]`}>FILTERS</span>
@@ -52,13 +52,15 @@ export default function FilterControlsMini({ lang, gender, searchParams, filtere
           />
         </div>
 
+        <div className="h-auto bg-t_white"></div>
+
         <div className={`bg-t_mustard p-[0.6rem] sm:p-5 px-5 sm:px-10 fixed w-full z-[90] bottom-0 text-[13px] ${albert_500.className}`}>
           <div className="max-w-[800px] mx-auto flex justify-between gap-6">
             <SaveFilterButton className="w-1/2 border-t_black border-[0.1rem]"
               font={`${albert_500.className} text-[13px]`}
             />
 
-            <button className="bg-t_black text-t_white rounded-full w-1/2"
+            <button className="bg-t_black text-t_white sm:py-4 rounded-full w-1/2"
               onClick={closeFilters}
             >
               VIEW {filteredMatchesTotal} RESULTS
