cf5ee444c4765329b8c876d82153816d7f001f87
waikoo <beroer@protonmail.com>
2024-07-18 16:41:22 +0300

    Style: make page content wider on /products

--- app/[lang]/(client)/[gender]/products/page.tsx
+++ app/[lang]/(client)/[gender]/products/page.tsx
@@ -24,7 +24,7 @@ export default async function Page({ params: { lang, gender }, searchParams }: P
         alt="four women crossing the street, each in a different outfit, cropped from waist down"
         className="xl:hidden w-screen mt-3"
       />
-      <main className="bg-t_white dark:bg-t_black text-content mx-auto px-[12px] sm:px-[55px] xl:px-20 lg:max-w-[1500px]">
+      <main className="bg-t_white dark:bg-t_black text-content mx-auto px-[12px] sm:px-[55px] xl:px-0 lg:max-w-[1500px] 3xl:max-w-[1800px]">
         <img src="/images/products/crossing.png"
           alt="four women crossing the street, each in a different outfit, only legs are visible"
           className="hidden xl:block xl:my-3 xl:w-full"
