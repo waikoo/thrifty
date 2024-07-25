052e96cd69d6772c3c217a91e621c7db3a1a31ec
waikoo <beroer@protonmail.com>
2024-07-26 01:02:12 +0300

    Add: loading indicator on page load for all pages

---
+++ app/[lang]/(client)/[gender]/products/loading.tsx
@@ -0,0 +1,7 @@
+import PageIsLoading from "@/app/components/generic/PageLoading";
+
+export default function Loading() {
+
+  return <PageIsLoading />
+}
+
---
+++ app/components/generic/PageLoading.tsx
@@ -0,0 +1,11 @@
+import Spinner from "@/app/components/generic/Spinner";
+
+export default function PageIsLoading() {
+
+  return (
+    <div className="grid h-screen place-items-center">
+      <Spinner />
+    </div>
+  )
+}
+
