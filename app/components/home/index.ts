4af842a04a5e0a3b1a46973758b8e9bc85e0b82f
waikoo <beroer@protonmail.com>
2024-07-26 00:40:32 +0300

    Remove: /home/index.ts

--- app/components/home/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as NewsletterSubscription } from './NewsletterSubscription'
-export { default as NewArrivalsControls } from './NewArrivalsControls'
-export { default as NewArrivalsGrid } from './NewArrivalsGrid'
