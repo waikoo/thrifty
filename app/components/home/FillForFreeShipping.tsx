2eebf91647ebfee6c28ae0bc955673cb38e7d435
waikoo <beroer@protonmail.com>
2024-07-27 19:25:25 +0300

    Change: if it's a free delivery, don't show FillForFreeShipping

--- app/components/home/FillForFreeShipping.tsx
+++ app/components/home/FillForFreeShipping.tsx
@@ -14,6 +14,7 @@ import { useCartStore } from "@/state/client/cartState"
 import { useEffect, useState } from 'react';
 import { supabase } from '@/app/supabase';
 import ProductItem from '../products/ProductItem';
+import { useOrderStore } from "@/state/client/orderState";
 
 type NewArrivalsProps = {
   lang: Locales
@@ -22,6 +23,7 @@ type NewArrivalsProps = {
 
 export default function FillForFreeShipping({ lang, gender = 'women' }: NewArrivalsProps) {
   const { cart } = useCartStore()
+  const { isFreeDelivery } = useOrderStore()
 
   const [accessories, setAccessories] = useState<ProductItemType[] | null>([])
 
@@ -57,7 +59,7 @@ export default function FillForFreeShipping({ lang, gender = 'women' }: NewArriv
     });
   }, [cart]);
 
-  return cart.length === 0 ? null : (
+  return cart.length === 0 || isFreeDelivery ? null : (
     <section className={`bg-t_white w-full flex flex-col pb-10`}>
       <div className={`${'mx-auto w-[80%]'}`}>
         <h3 className={`text-bkg py-10 text-2xl font-bold text-content text-center text-[1rem]`}>
