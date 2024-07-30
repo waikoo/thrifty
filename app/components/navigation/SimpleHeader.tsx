ee706c30817e3c59f81ebddad2f9a7a46f513189
waikoo <beroer@protonmail.com>
2024-07-30 16:49:22 +0300

    Change: SimpleHeader to mustard theme

--- app/components/navigation/SimpleHeader.tsx
+++ app/components/navigation/SimpleHeader.tsx
@@ -1,49 +1,42 @@
-"use client"
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
+import { MdArrowBackIosNew } from 'react-icons/md'
+import { RxCross1 } from 'react-icons/rx'
+
 import { Logo, WithHome } from '@/app/components/navigation/'
-import NavIcons from "@/app/components/navigation/NavIcons"
-import IconSearch from '@/app/components/navigation/icons/IconSearch'
-import { useEffect, useRef, useState } from 'react'
+import { albert_800 } from '@/utils/fonts'
 
 export default function SimpleHeader() {
-  const [showSearchBar, setShowSearchBar] = useState(false)
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  const handleSearchClick = () => {
-    setShowSearchBar(!showSearchBar)
-  }
-
-  useEffect(() => {
-    if (showSearchBar) {
-      inputRef.current?.focus()
-    }
-
-  }, [showSearchBar])
+  const router = useRouter()
 
   return (
-    <nav className="">
-      <div className="mx-auto flex h-16 max-w-[85vw] items-center justify-between p-3">
-        <WithHome> <Logo className="" /> </WithHome>
-
-        <div className="flex w-[30vw] items-center gap-8">
-
-          <section className={`text-bkg relative top-1 flex w-full items-center gap-2`}>
-            {showSearchBar && (
-              <input
-                type="search"
-                placeholder="Search"
-                className={`text-content placeholda w-full appearance-none self-start border-b-[0.01rem] border-l-0 border-r-0 border-t-0 bg-transparent p-0 outline-0 ring-0 focus:outline-0 focus:ring-0`}
-                style={{ WebkitAppearance: 'none' }}
-                ref={inputRef}
-              />)}
-
-            <button className="ml-auto cursor-pointer"
-              onClick={handleSearchClick}>
-              <IconSearch className="" />
-            </button>
-          </section>
-
-          <NavIcons className="" />
+    <nav className="bg-t_black w-screen">
+      <div className="grid grid-cols-[auto_1fr_auto] p-4">
+        <div onClick={() => router.back()}>
+          <MdArrowBackIosNew
+            color="white"
+            className="cursor-pointer"
+          />
         </div>
+
+        <WithHome>
+          <Logo
+            logoColor="#d2d62e"
+            width="8rem"
+          />
+        </WithHome>
+
+        <Link
+          href={`/`}
+          className="cursor-pointer"
+        >
+          <RxCross1 color="white" />
+        </Link>
+      </div>
+
+      <div className={`bg-t_mustard text-[17px] text-center p-1 ${albert_800.className}`}>
+        CHECKOUT
       </div>
     </nav>
   )
