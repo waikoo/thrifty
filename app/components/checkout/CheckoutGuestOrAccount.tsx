4f57a5eb500c121247ca5346824bed7926996b7d
waikoo <beroer@protonmail.com>
2024-07-31 19:17:28 +0300

    Style: use conditional bold font
    continue as guest is selected if login or create account operation is
    dismissed

--- app/components/checkout/CheckoutGuestOrAccount.tsx
+++ app/components/checkout/CheckoutGuestOrAccount.tsx
@@ -1,13 +1,38 @@
+"use client"
+import { useEffect, useRef, useState } from 'react'
+
 import { borderRadius } from '@/app/components/data/universalStyles'
 import { useUIStore } from '@/state/client/uiState'
-import { albert_600 } from '@/utils/fonts'
+import { albert, albert_600 } from '@/utils/fonts'
 
 export default function CheckoutGuestOrAccount() {
-  const { setShowSignIn } = useUIStore()
+  const { showSignIn, setShowSignIn } = useUIStore()
+  const inputRef1 = useRef<HTMLInputElement>(null)
+  const inputRef2 = useRef<HTMLInputElement>(null)
+  const [fontWeight1, setFontWeight1] = useState(albert_600.className)
+  const [fontWeight2, setFontWeight2] = useState(albert.className)
+
+  const onChange = () => {
+    if (inputRef1.current?.checked) {
+      setFontWeight1(albert_600.className)
+      setFontWeight2(albert.className)
+    }
+    if (inputRef2.current?.checked) {
+      setFontWeight1(albert.className)
+      setFontWeight2(albert_600.className)
+      setShowSignIn(true)
+    }
+  }
+
+  useEffect(() => {
+    if (!showSignIn) {
+      inputRef1.current?.click()
+    }
+  }, [showSignIn])
 
   return (
     <div className={`bg-white w-[800px] p-4 ${borderRadius}`}>
-      <div className="mx-auto w-1/2 flex justify-around">
+      <div className="mx-auto w-1/2 flex justify-around sm:text[17px] xl:text-[14px]">
         <label
           htmlFor="guest"
           className="flex gap-2"
@@ -16,10 +41,12 @@ export default function CheckoutGuestOrAccount() {
             type="radio"
             name="guestOrAccount"
             id="guest"
-            className="checked:bg-black"
+            className={`checked:bg-black`}
             defaultChecked={true}
+            ref={inputRef1}
+            onChange={onChange}
           />
-          <span className={`text-[14px] ${albert_600.className}`}>
+          <span className={`${fontWeight1}`}>
             Continue as Guest
           </span>
         </label>
@@ -32,10 +59,12 @@ export default function CheckoutGuestOrAccount() {
             type="radio"
             name="guestOrAccount"
             id="account"
-            className="checked:bg-black"
-            onClick={() => setShowSignIn(true)}
+            className={`checked:bg-black`}
+            // onClick={() => setShowSignIn(true)}
+            onChange={onChange}
+            ref={inputRef2}
           />
-          <span className={`text-[14px] ${albert_600.className}`}>
+          <span className={`${fontWeight2}`}>
             Log In / Create Account
           </span>
         </label>
