902a2b2d8fa8b96aae91eff5ceb1a8c0731d307a
waikoo <beroer@protonmail.com>
2024-08-04 16:20:45 +0300

    Change: error & checked state icons to custom ones

--- app/components/checkout/CheckMark.tsx
+++ app/components/checkout/CheckMark.tsx
@@ -1,9 +1,20 @@
-import { FaCheck } from "react-icons/fa";
-
 export default function CheckMark() {
+
   return (
-    <div className="bg-[#6ca648] z-50 grid h-[1.875rem] w-[1.875rem] place-items-center rounded-full">
-      <FaCheck color="white" size="0.9rem" className="z-50" />
-    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="28"
+      height="28"
+      fill="none"
+      viewBox="0 0 28 28"
+      className="bg-t_white"
+    >
+      <path
+        fill="#6CA648"
+        fillRule="evenodd"
+        d="M14 28c7.732 0 14-6.268 14-14S21.732 0 14 0 0 6.268 0 14s6.268 14 14 14zm7.348-16.732a1.68 1.68 0 10-2.376-2.376l-6.652 6.652-3.292-3.292a1.68 1.68 0 10-2.376 2.376l4.48 4.48a1.68 1.68 0 002.376 0l7.84-7.84z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
   )
 }
--- app/components/checkout/CheckoutError.tsx
+++ app/components/checkout/CheckoutError.tsx
@@ -1,11 +1,20 @@
-import { MdError } from 'react-icons/md'
 
 export default function CheckoutError() {
 
   return (
-    <div>
-      <MdError color="red" className="bg-white h-[1.875rem] w-[1.875rem] rounded-full" />
-    </div>
-  )
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="28"
+      height="28"
+      fill="none"
+      viewBox="0 0 28 28"
+    >
+      <rect width="28" height="28" fill="#F2F2F2" rx="14"></rect>
+      <path
+        fill="#E80000"
+        d="M14 28a14 14 0 100-28 14 14 0 000 28zm-2.1-7.7a2.1 2.1 0 114.2 0 2.1 2.1 0 01-4.2 0zm.722-13.552a1.4 1.4 0 012.756 0L15.4 7v7l-.022.252a1.4 1.4 0 01-2.756 0L12.6 14V7l.022-.252z"
+      ></path>
+    </svg>
+  );
 }
 
