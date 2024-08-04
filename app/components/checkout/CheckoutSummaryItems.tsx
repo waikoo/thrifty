c66859df606333cc7fb3b896eefee6c4a5f24f1e
waikoo <beroer@protonmail.com>
2024-08-04 16:27:52 +0300

    Fix: shipping type didn't show up on /summary if it was a store pickup

--- app/components/checkout/CheckoutSummaryItems.tsx
+++ app/components/checkout/CheckoutSummaryItems.tsx
@@ -1,15 +1,17 @@
 "use client"
 import CheckoutSummaryItem from "@/app/components/checkout/CheckoutSummaryItem";
 import { useCheckoutStore } from "@/state/client/checkoutState";
+import { useOrderStore } from "@/state/client/orderState";
 
 
 export default function CheckoutSummaryItems() {
   const { firstName, lastName, email, phone, address, city, country, zipcode, paymentType } = useCheckoutStore()
+  const { shippingType } = useOrderStore()
 
   return (
     <div className="flex flex-col gap-[3rem]">
       <CheckoutSummaryItem title="CONTACT" content={[firstName, lastName, phone, email]} />
-      <CheckoutSummaryItem title="SHIPPING" content={[address, city, country, zipcode]} />
+      <CheckoutSummaryItem title="SHIPPING" content={shippingType === 'store' ? [shippingType] : [address, city, country, zipcode]} />
       <CheckoutSummaryItem title="PAYMENT" content={[paymentType]} />
     </div>
   )
