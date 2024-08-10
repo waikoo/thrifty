1339475664d38da28b4b8a059017844d17d3c195
waikoo <beroer@protonmail.com>
2024-08-10 17:14:01 +0300

    Add: opacity-50 only when isBlockHidden is true in CartPaymentMethods

--- app/components/cart/CartPaymentMethods.tsx
+++ app/components/cart/CartPaymentMethods.tsx
@@ -15,6 +15,7 @@ const inter = Inter({ subsets: ['latin'], display: 'swap', weight: '600' })
 
 export default function CartPaymentMethods({ className, isBlockHidden }: CartPaymentMethodsProps) {
   const textStyle = isBlockHidden ? 'text-t_black/50 dark:text-t_white/50' : 'text-t_black dark:text-t_white'
+  const halfOpacityOnHidden = isBlockHidden ? 'opacity-50' : '';
 
   return (
     <div className={`mt-3 flex flex-col gap-2 p-4 ${className}`}>
@@ -23,9 +24,9 @@ export default function CartPaymentMethods({ className, isBlockHidden }: CartPay
       </p>
 
       <div className="flex items-center gap-2">
-        <RiVisaLine size="2.6rem" color="#254AA5" className="opacity-50" />
-        <IconMasterCard className="opacity-50" />
-        <FaApplePay size="2.6rem" className="opacity-50" />
+        <RiVisaLine size="2.6rem" color="#254AA5" className={halfOpacityOnHidden} />
+        <IconMasterCard className={halfOpacityOnHidden} />
+        <FaApplePay size="2.6rem" className={halfOpacityOnHidden} />
 
         <div className={`${textStyle} grid place-items-center text-center text-[7px] sm:text[12px] xl:text-[8px] ${inter.className}`}>
           <span className="bg-t_white dark:t_black m-0 w-10 border-[#d8d8d8] border-[0.1rem] p-0">
