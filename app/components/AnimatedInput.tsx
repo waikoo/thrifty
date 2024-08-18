0134d5f70bf7d728989cd5543d638bdeb6e67abd
waikoo <beroer@protonmail.com>
2024-08-18 20:26:17 +0300

    Change: NewsletterSubscription input field to AnimatedInput

--- app/components/AnimatedInput.tsx
+++ app/components/AnimatedInput.tsx
@@ -17,7 +17,7 @@ export default function AnimatedInput({ type, id, placeholder, value, onChange, 
   return (
     <div className="relative w-full">
       <input
-        className={tm(`${className} bg-t_white border-b-[#9d9d9d] peer w-full border-x-0 border-b-[0.1rem] border-t-0 border-solid pl-0 ${placeholderStyle} focus:border-sky-500 focus:outline-none focus:ring-0`)}
+        className={tm(`${className} bg-transparent border-b-[#9d9d9d] peer w-full border-x-0 border-b-[0.1rem] border-t-0 border-solid pl-0 ${placeholderStyle} focus:border-sky-500 focus:outline-none focus:ring-0`)}
         id={id}
         type={type}
         placeholder={placeholder}
--- app/components/home/NewsletterSubscription.tsx
+++ app/components/home/NewsletterSubscription.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { albert, albert_500 } from "@/utils/fonts"
+import { albert_500 } from "@/utils/fonts"
+import AnimatedInput from "@/app/components/AnimatedInput"
 
 export default function NewsletterSubscription() {
 
@@ -8,12 +9,7 @@ export default function NewsletterSubscription() {
       <h3 className="font-allenoire max-w-[300px] text-center text-[1.5rem] md:text-[2.1875rem] xl:text-[3.125rem] whitespace-nowrap flex flex-col items-center">subscribe to our <span className="-mt-2">newsletter</span></h3>
 
       <form className="flex flex-col gap-7 w-[80%] max-w-[400px] xl:max-w-[350px]">
-        <input
-          type="text"
-          placeholder="Enter your email address"
-          className={`bg-bkg text-content flex-grow p-2 border-0 bg-transparent border-b-[0.12rem] border-t_black focus:outline-0 ${albert.className} text-[0.8125rem] md:text-[1rem] tracking-wide text-[#484848]`}
-          spellCheck="false"
-        />
+        <AnimatedInput type="text" id="newsletter" placeholder="Enter your email address" value="" onChange={() => { }} />
         <button className={`bg-t_black text-t_white rounded-full p-3 text-[0.8125rem] tracking-wide xl:max-w-[70%] xl:px-8 xl:py-2 xl:mt-3 xl:mx-auto ${albert_500.className}`}>SUBSCRIBE</button>
       </form>
     </section>
