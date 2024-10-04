432d7f7a0e3df3614b48767182450804432f96f3
waikoo <beroer@protonmail.com>
2024-10-04 22:04:51 +0200

    Fix: cls due to hamburger icon being rendered before styles apply

--- app/components/navigation/NavBar.tsx
+++ app/components/navigation/NavBar.tsx
@@ -30,9 +30,7 @@ const NavBar = ({ className }: NavBarProps) => {
   }
   const noBorderOnScroll = position === 'static' ? `${borderColor[theme]} xl:border-b-2` : ''
   const currentViewport = useViewport()
-  const showHamburgerIcon = currentViewport < viewport.xl
   const logoWidth = currentViewport < viewport['2xl'] ? '11.6875rem' : '8.1875rem'
-  const hamburgerDistance = showHamburgerIcon ? 'ml-4' : ''
   const isTablet = currentViewport >= viewport.sm && currentViewport < viewport.xl
   const { showMobileSearch, setShowMobileSearch, showHamburgerMenu, setShowHamburgerMenu } = useNavigationStore()
   const isTabletAndProductsEndpoint = isTablet && pathname.split('/')[3] === 'products'
@@ -47,20 +45,18 @@ const NavBar = ({ className }: NavBarProps) => {
       <div className={`${noBorderOnScroll} relative grid w-screen grid-cols-[2rem_4rem_1fr_auto_auto] xl:grid-cols-3 pb-2 pt-4 mx-auto max-w-[90vw] 3xl:max-w-[1800px]`}
         onMouseEnter={() => setShowGenderMenu(false)} // makes categorymenu disappear when exiting with mouseover on top
       >
-        {showHamburgerIcon &&
-          <div className="w-5 h-5 mr-5 self-end col-start-1 col-end-2 row-start-1"
-            onClick={() => setShowHamburgerMenu(!showHamburgerMenu)}>
-            <IconHamburger width="27px" />
-          </div>
-        }
+        <div className="w-5 h-5 mr-5 self-end col-start-1 col-end-2 row-start-1 md:hidden"
+          onClick={() => setShowHamburgerMenu(!showHamburgerMenu)}>
+          <IconHamburger width="27px" />
+        </div>
 
         {isTablet ? (
-          <div className={`self-end ${hamburgerDistance} col-start-2 col-end-3 xl:col-start-1 xl:col-end-2 row-start-1`}
+          <div className={`self-end md:ml-4 col-start-2 col-end-3 xl:col-start-1 xl:col-end-2 row-start-1`}
             onClick={handleShowSearch}>
             <IconSearch />
           </div>
         ) : (
-          <SearchBar className={`self-end ${hamburgerDistance} col-start-2 col-end-3 xl:col-start-1 xl:col-end-2 row-start-1`} />
+          <SearchBar className={`self-end md:ml-4 col-start-2 col-end-3 xl:col-start-1 xl:col-end-2 row-start-1`} />
         )}
 
         {position === 'static' ? (
