507562ec0beb3e063cdd51313a4a933b5280e7d6
waikoo <beroer@protonmail.com>
2024-10-06 01:05:35 +0200

    Style: push HeroTextNewIn against edge

--- app/components/home/HeroTextNewIn.tsx
+++ app/components/home/HeroTextNewIn.tsx
@@ -1,9 +1,9 @@
 export default function HeroTextNewIn() {
 
   return (
-    <div className="grid place-items-center">
-      <p className="mt-[12rem] sm:mt-[20rem] 2xl:mt-32 xl:mt-10 ml-auto h-full w-full xl:w-1/3 text-center inset-0 font-alokary absolute text-[40px] md:text-[90px] xl:text-[90px] 2xl:text-[100px] leading-none text-t_purple text-shadow shadow-black tracking-widest">
+    <p className="right-0 xl:w-1/3 text-center z-[200] font-alokary absolute text-[40px] md:text-[90px] xl:text-[90px] 2xl:text-[100px] leading-none text-white text-shadow shadow-black tracking-widest flex items-center h-full justify-end mr-[-18px]">
 
+      <div>
         <span className="sm:block -mb-2 md:mb-7">
           RADI
           <span className="sm:block sm:mt-3 md:mt-7 xl:mt-8">ANT</span>
@@ -13,8 +13,8 @@ export default function HeroTextNewIn() {
           REVI
           <span className="sm:block sm:mt-3 md:mt-7 xl:mt-10">VAL</span>
         </span>
-      </p>
-    </div>
+      </div>
+    </p>
   )
 }
 
