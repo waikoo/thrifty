72c4c5cfc487d362863e89f6294f989bbf4168db
waikoo <beroer@protonmail.com>
2025-02-19 16:33:25 +0100

    Style: change font size on desktop

--- app/[lang]/(client)/[gender]/returns/page.tsx
+++ app/[lang]/(client)/[gender]/returns/page.tsx
@@ -3,7 +3,7 @@ import { albert } from "@/utils/fonts";
 export default function Page() {
 
   return (
-    <div className={`text-content my-20 ${albert.className} text-[13px] sm:text-[17px] xl:text-[11px] text-center`}>No returns were found.</div>
+    <div className={`text-content my-20 ${albert.className} text-[13px] sm:text-[17px] xl:text-[14px] text-center`}>No returns were found.</div>
   )
 }
 
