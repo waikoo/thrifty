4fec536f0066e0d687f5bc134089f6f5a81d0d32
waikoo <beroer@protonmail.com>
2025-02-20 00:37:21 +0100

    Add: type for window.google

--- app/components/navigation/SignInWithGoogle.tsx
+++ app/components/navigation/SignInWithGoogle.tsx
@@ -4,6 +4,22 @@ import { albert_600 } from '@/utils/fonts';
 import { useEffect } from "react";
 import { supabase } from "@/app/supabase";
 
+declare global {
+  interface Window {
+    google?: {
+      accounts: {
+        id: {
+          initialize: (config: {
+            client_id: string;
+            callback: (response: { credential: string }) => void;
+          }) => void;
+          renderButton: (element: HTMLElement | null, options: { theme: string; size: string }) => void;
+        };
+      };
+    };
+  }
+}
+
 type SignInWithGoogleProps = {
   setShowSignIn: (show: boolean) => void
 }
