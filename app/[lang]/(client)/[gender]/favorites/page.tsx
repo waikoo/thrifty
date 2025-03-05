223e3d34808e4d7fe34f29b2811c1d008c2c5cd5
waikoo <beroer@protonmail.com>
2025-03-05 21:17:14 +0100

    Style: add bottom margin to favorites and cart pages

--- app/[lang]/(client)/[gender]/favorites/page.tsx
+++ app/[lang]/(client)/[gender]/favorites/page.tsx
@@ -12,7 +12,7 @@ type PageProps = {
 export default async function Page({ params: { lang, gender }, searchParams, }: PageProps) {
 
   return (
-    <main className="bg-white dark:bg-black text-t_black dark:text-t_white mx-auto mt-6 flex w-full max-w-[1600px] flex-col rounded-[35px] items-center lg:max-w-[1500px]">
+    <main className="bg-white dark:bg-black text-t_black dark:text-t_white mx-auto mt-6 flex w-full max-w-[1600px] flex-col rounded-[35px] items-center lg:max-w-[1500px] mb-[60px]">
       <FavoritesOrCart lang={lang} gender={gender} />
 
       <FavoritesContent />
