76dec38228e4eb144962b310ebb33963eb37fe58
waikoo <beroer@protonmail.com>
2025-03-06 14:16:01 +0100

    Fix: sign in popup appears when click on sign in inside hamburgermenu

--- app/components/navigation/HamburgerAccountBar.tsx
+++ app/components/navigation/HamburgerAccountBar.tsx
@@ -8,9 +8,11 @@ import { albert_600 } from "@/utils/fonts";
 import { useNavigationStore } from "@/state/client/navigationState";
 import IconAccount from "@/app/components/navigation/icons/IconAccount";
 import { getLocalPartOfEmail } from "@/utils/navigation";
+import { useUIStore } from "@/state/client/uiState";
 
 export default function HamburgerAccountBar() {
   const { setShowHamburgerMenu } = useNavigationStore()
+  const { showSignIn, setShowSignIn } = useUIStore()
   const [text, setText] = useState('')
 
   useEffect(() => {
@@ -29,11 +31,17 @@ export default function HamburgerAccountBar() {
 
   }, [])
 
+  const handleClick = () => {
+    if (text !== 'Sign In') return
+    setShowSignIn(true)
+    setShowHamburgerMenu(false)
+  }
+
   return (
     <div className="grid grid-cols-2 w-full text-t_black dark:text-t_white">
       <div className="flex gap-2 items-center">
         <IconAccount width="15px" />
-        <span className={`${albert_600.className} text-[0.75rem] sm:text-[1rem] py-2`}>{text}</span>
+        <span className={`${albert_600.className} text-[0.75rem] sm:text-[1rem] py-2`} onClick={handleClick}>{text}</span>
       </div>
       <div
         onClick={() => setShowHamburgerMenu(false)}
