2a18d6c736ce42362d37f08401f09473f22f4c9a
waikoo <beroer@protonmail.com>
2025-03-07 00:47:41 +0100

    Style: add font family & weight

--- app/components/favorites/EmptyFavorites.tsx
+++ app/components/favorites/EmptyFavorites.tsx
@@ -1,4 +1,5 @@
 import useViewport from '@/app/components/hooks/useViewport'
+import { albert, albert_800 } from '@/utils/fonts'
 
 export default function EmptyFavorites() {
   const viewportWidth = useViewport()
@@ -6,12 +7,12 @@ export default function EmptyFavorites() {
 
   return (
     <div>
-      <div className="text-content w-screen py-20 text-center tracking-wider">
-        <h4 className="mb-[1.375rem] text-[17px] sm:text-[21px] xl:text-[18px] font-extrabold">
+      <div className="text-content w-screen py-20 px-20 text-center tracking-wider h-[calc(80dvh)]">
+        <h4 className={`mb-[1.375rem] text-[17px] sm:text-[21px] xl:text-[18px] ${albert_800.className}`}>
           No favorites yet.
         </h4>
 
-        <span className="font-semibold text-[14px] sm:text-[17px] xl:text-[14px]">
+        <span className={`${albert.className} text-[14px] sm:text-[17px] xl:text-[14px]`}>
           {tapOrClick} the heart icon to keep track of the items you love!
         </span>
       </div>
