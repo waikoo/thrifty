90a309f61c2bb74045e47ffb7db3ecf4eebfdba6
waikoo <beroer@protonmail.com>
2025-03-07 22:42:38 +0100

    Style: add font weight and family to footer

--- app/components/footer/FooterPhoneAndEmail.tsx
+++ app/components/footer/FooterPhoneAndEmail.tsx
@@ -1,5 +1,6 @@
 import Email from "@/app/components/footer/icons/Email";
 import Phone from "@/app/components/footer/icons/Phone";
+import { albert_500 } from "@/utils/fonts";
 
 type FooterPhoneAndEmailProps = {
   invert?: boolean
@@ -9,7 +10,7 @@ export default function FooterPhoneAndEmail({ invert }: FooterPhoneAndEmailProps
   const invertedStyle = invert ? 'text-t_white' : 'text-t_black'
 
   return (
-    <div className={`flex flex-col gap-3 ${invertedStyle}`}>
+    <div className={`flex flex-col gap-3 ${albert_500.className} ${invertedStyle}`}>
       <div className="flex items-center gap-2 underline underline-offset-2">
         <Phone invert={invert} />
         <span>1234567890</span>
--- app/components/footer/FooterTitle.tsx
+++ app/components/footer/FooterTitle.tsx
@@ -1,10 +1,12 @@
+import { albert_800 } from "@/utils/fonts"
+
 type FooterTitleProps = {
   children: React.ReactNode
 }
 
 export default function FooterTitle({ children }: FooterTitleProps) {
   return (
-    <h3 className="footerTitles text-t_mustard">{children}</h3>
+    <h3 className={`footerTitles ${albert_800.className} text-t_mustard`}>{children}</h3>
   )
 }
 
